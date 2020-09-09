31f88e4afa9e52427e4aca0d0904dd148fc27814
Layla Marchant <diligentcircle@riseup.net>
2020-09-08 22:06:35 -0400

    Fixed datapath.lua.

--- extras/windows/installer/datapath.lua
+++ extras/windows/installer/datapath.lua
@@ -1,3 +1,3 @@
--- This file specifies the directory where Naev's user data will be stored.
--- By default, the path is relative to the directory the Naev binary is in.
-datapath = "naev-data/"
+-- This file specifies the directory where Naikari's user data will be stored.
+-- By default, the path is relative to the directory the Naikari binary is in.
+datapath = "naikari-data/"
