74822f909d217ef5bb8c759b245f58ff11c2b890
Layla Marchant <diligentcircle@riseup.net>
2021-03-01 15:23:23 -0500

    Removed advertises, Minerva Station, and Strangelove Lab.
    Also removed minigame scripts as we won't be using those in Naikari.

--- dat/factions/spawn/civilian.lua
+++ dat/factions/spawn/civilian.lua
@@ -5,37 +5,14 @@ require("factions/spawn/common")
 function spawn_patrol ()
     local pilots = {}
 
-    -- Compute the hostile presence
-    local host = 0
-    for k,fact in pairs(faction.get("Civilian"):enemies()) do 
-       host = host + system.cur():presence(fact)
-    end
-    host = host / system.cur():presence(faction.get("Civilian"))
-
-    -- The more hostiles, the less advertisers
-    local prop = .25 -- Advertisers proportion at host = 0
-    local h0   = 3  -- At this hostile presence, advertiser prop is 5% of original proportion
-    local r = rnd.rnd() + prop*(1-math.exp(-3*host/h0))
-
-    if r < prop then
-       local civships = {{"Advertiser Schroedinger", 8},
-                         {"Advertiser Llama", 8},
-                         {"Advertiser Gawain", 8},
-                         {"Advertiser Hyena", 13}
-                        }
-       local select = rnd.rnd(1, #civships)
-       scom.addPilot( pilots, civships[select][1], civships[select][2] );
-    else
-       local adships = {{"Civilian Schroedinger", 8},
-                         {"Civilian Llama", 8},
-                         {"Civilian Gawain", 8},
-                         {"Civilian Hyena", 13}
-                        }
-       local select = rnd.rnd(1, #adships)
-       scom.addPilot( pilots, adships[select][1], adships[select][2] );
-    end
-    
-
+    local ships = {
+        {"Civilian Schroedinger", 8},
+        {"Civilian Llama", 8},
+        {"Civilian Gawain", 8},
+        {"Civilian Hyena", 13}
+    }
+    local i = rnd.rnd(1, #ships)
+    scom.addPilot( pilots, ships[i][1], ships[i][2] );
 
     return pilots
 end
