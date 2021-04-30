2c2caa2c32d3ea10d35e5528ca4a105d6de23c68
Layla Marchant <diligentcircle@riseup.net>
2021-04-30 19:59:54 -0400

    Revert "Tweaked structure/weapon outfit slot colour and show it in the alt text."
    This reverts commit 4890d3e23989bd95418ba3d5da65f84379d99a2c.

--- src/land_outfits.c
+++ src/land_outfits.c
@@ -625,10 +625,7 @@ ImageArrayCell *outfits_imageArrayCells( Outfit **outfits, int *noutfits )
             l = strlen(o->desc_short) + 128;
             coutfits[i].alt = malloc( l );
             p  = scnprintf( &coutfits[i].alt[0], l, "%s\n", _(o->name) );
-            if (o->slot.type != OUTFIT_SLOT_NA)
-               p += scnprintf( &coutfits[i].alt[p], l-p, _("#%c%s #%c%s #0slot\n"),
-                     outfit_slotSizeColourFont(&o->slot), outfit_slotSize(o),
-                     outfit_slotTypeColourFont(&o->slot), outfit_slotName(o) );
+            p += scnprintf( &coutfits[i].alt[p], l-p, "#%c%s#0\n", outfit_slotSizeColourFont(&o->slot), outfit_slotSize(o) );
             if (outfit_isProp(o, OUTFIT_PROP_UNIQUE))
                p += scnprintf( &coutfits[i].alt[p], l-p, _("#oUnique#0\n") );
             if (o->slot.spid!=0)
