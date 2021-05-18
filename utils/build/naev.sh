80600b3a83679bc9e0d3d9956aae4f3853db70d2
Layla Marchant <diligentcircle@riseup.net>
2021-05-18 01:39:42 -0400

    Fixed up naev.sh.

--- utils/build/naev.sh
+++ utils/build/naev.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 NAEV="@naev_bin@ -d @source_root@/dat -d @source_root@/artwork -d @build_root@/dat -d @source_root@"
 
-"@source_root@/meson.sh" compile -C "@build_root@" naev-gmo
+"@source_root@/meson.sh" compile -C "@build_root@" naikari-gmo
 for mo_path in "@build_root@"/po/*.gmo; do
     mo_name="$(basename "$mo_path")"
     lang=${mo_name%.gmo}
     mkdir -p "@build_root@"/dat/gettext/$lang/LC_MESSAGES
-    cp -v "$mo_path" "@build_root@"/dat/gettext/$lang/LC_MESSAGES/naev.mo
+    cp -v "$mo_path" "@build_root@"/dat/gettext/$lang/LC_MESSAGES/naikari.mo
 done
-grep -q '%<PRI' "@build_root@"/po/*.gmo && echo "***WARNING: Naev can't translate 'sysdep' strings like %<PRIu64>. Try %.0f?"
+grep -q '%<PRI' "@build_root@"/po/*.gmo && echo "***WARNING: Naikari can't translate 'sysdep' strings like %<PRIu64>. Try %.0f?"
 
 type "gdb" > /dev/null
 if [ "$?" == 0 ]; then
