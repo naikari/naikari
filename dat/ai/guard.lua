0a1d20944eefa7e46f81ee0a019e3d462874b95d
Edgar Simo-Serra <bobbens@gmail.com>
2021-06-25 22:49:45 +0900

    Guard AI runs create_post()

--- dat/ai/guard.lua
+++ dat/ai/guard.lua
@@ -20,6 +20,9 @@ mem.enemyclose    = mem.guarddodist
 function create ()
    -- Choose attack format
    attack_choose()
+
+   -- Finish up creation
+   create_post()
 end
 
 local function gdist( t )
