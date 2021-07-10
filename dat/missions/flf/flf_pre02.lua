70a0b969039553f4154829770c446cbd5d208a33
Layla Marchant <diligentcircle@riseup.net>
2021-07-10 18:53:20 -0400

    A little more progress on pre02

--- dat/missions/flf/flf_pre02.lua
+++ dat/missions/flf/flf_pre02.lua
@@ -51,30 +51,30 @@ answer_text.help = _([[You tell the man that you have heard of the struggles of 
 
 "Let's get one thing straight: you're not going to be the one to fly in and cure all our problems any more than you're going to fly in and destroy us. We've been working hard to put a stop to the Dvaered oppression for a long time now. If you have any part at all in finally defeating the Dvaereds, it will be on the back of the many of us who have fought and died already, who collectively built this guerrilla force."
 
-He sighs once again, then stands up. "That said, if you really do want to help, you need to understand our struggles properly." You look back into his eyes, and you notice that they've changed somewhat. While they still convey a great degree of distrust, they don't seem to convey outright hostility as they previously did. He continues. "Let me show you personally what's going on in the Frontier. What do you say?"]])
+He sighs once again, then stands up. "That said, if you really do want to help, you need to understand our struggles properly." You look back into his eyes and see a distrustful, yet somewhat less hostile, glare. "Will you let me show you personally what's going on in the Frontier?"]])
 answer_text.neutral = _([[You tell the man that you are just a fellow pilot who helped someone in need. You say that you are neutral on the conflict between House Dvaered and the FLF and promise not to rat out the FLF to the authorities. He frowns and his face becomes filled with rage. "Neutral?! How could you be neutral in this conflict? Do you have any idea what the Dvaereds do to us day after day in the Frontier? What they aspire to do if only we would get out of the way? Do you know nothing about our struggles?" You knew you weren't trusted, but you certainly didn't expect this level of anger. You open your mouth to answer, but not knowing what to say, you close it again and your eyes shift to the floor below you. Around you, you hear murmurs of agreement; it seems his outburst has attracted an audience, and this audience is firmly against you.
 
 The man lets out another sigh, then stands up. His expression has reverted again from one of intense anger to one of firm distrust. "I'll show you exactly what's going on in the Frontier, what the Dvaereds do to us, what the Dvaereds plan to do to us, and why we need to resist them. I'm sick and tired of outsiders like you who have no idea just how horrible the Dvaereds are." The offer takes you by surprise. You stand there silently, transfixed by the man's eyes which, now that you think about it, have a touch of pain and sadness to them. You consider his sudden offer. "Well?" he says, breaking you out of your trance. "Will you let me show you what's going on in the Frontier?"]])
 answer_text.learn = _([[You tell the man that you don't know much about the Frontier or the FLF, given your background having been raised in the Empire, and that you wish to learn more about the Frontier and the struggles of the FLF. As he raises an eyebrow, you add that you have no ill intent and won't tell anyone about Sindbad. The man pauses. "Odd. I never would have expected an Imperial to admit their own ignorance on the matter. Maybe there's hope for you after all.
 
-"I'll tell you what: why don't I help you out on what you're after? I can show you personally what's going on in the Frontier and what we do. We usually aren't in the business of educating people, but a genuine willingness to learn from an outsider is a real breath of fresh air. Are you ready right now?]])
+"I gotta say, though, if you really want to learn about the Frontier and what we're fighting for, this is entirely the wrong place for that." He pauses, then stands up. "I'll tell you what: this is a bit of a pain in the ass, but I don't have any important work to do at the moment, so I can guide you around the Frontier and show you what's at stake for us. Are you interested?"]])
 
 return_text = {}
 return_text.help = _([["I see you're back. Have you changed your mind about letting me show you what's going on in the Frontier?"]])
 return_text.neutral = return_text.help
-return_text.learn = _([["Oh, hello again. Are you ready to go on a tour of the Frontier now?"]])
+return_text.learn = _([["Oh, it's you. Have you changed your mind about accepting my offer to introduce the Frontier and its struggles to you?"]])
 
 response_no = {}
 response_no.help = _([[The man frowns, then silently returns to his seat and crosses his arms.]])
 response_no.neutral = response_no.help
-response_no.learn = _([[You thank the man, but say you are currently busy with something else. He sighs. "Well, that's understandable, I suppose. Feel free to talk to me later when you're ready."]])
+response_no.learn = _([[You thank the man, but say you are currently busy with something else. He sighs. "Ok," he says as he sits back down. "Let me know if you change your mind."]])
 
 response_yes = {}
-response_yes.help = _([[The man grins. "Perfect. I'll show you just what our stakes are, what we fight for, and what we're up against. And most importantly, I'll find out if we can really trust you.
+response_yes.help = _([[The man grins. "Perfect. I'll show you just what our stakes are, what we fight for, and what we're up against. And most importantly, maybe I can get you to chuck that shitty pretentiousness of yours out the window.
 
 "I suppose as good as place to start as any would be my homeworld, %s. Maybe that will give you the slightest inkling of how rich our society is. Too many people like you think that we're helpless barbarians who need to be uplifted and saved by an Imperial savior, and I'm sick and tired of that.
 
-"Oh, my name's Flint, by the way." You likewise tell Flint your own name. "I'll meet you at the landing bay. I would fly my own ship, but since you're not officially FLF yet, we'll have an easier time if I join you on your ship. Should avoid confrontations with the Dvaereds and allow us to get up-close without getting them on our tails. See you soon."
+"The name's Flint, by the way." You likewise tell Flint your own name. "I'll meet you at the landing bay. I would fly my own ship, but since you're not officially a part of the FLF, we'll have an easier time if I join you on your ship. Should avoid confrontations with the Dvaereds and allow us to get up close without getting them on our tails. See you soon."
 
 As Flint walks toward the landing bay, you ponder what exactly you did that made him so upset. You figure you'll find out soon enough from him, though, and resolve to listen as well as you can.]])
 response_yes.neutral = _([["Good. You'll see soon why being neutral in this conflict is unacceptable. More importantly, I'll find out if you can be trusted in the slightest.
@@ -84,17 +84,19 @@ response_yes.neutral = _([["Good. You'll see soon why being neutral in this conf
 "The name's Flint, by the way. Flint as in if you try to turn me over to the Dvaereds, I'll light a fire in your ass." You in turn tell Flint your own name, then reaffirm that you won't double-cross him. "Good. I'll meet you at the landing bay. I would fly my own ship, but you being neutral means that we can avoid detection from the Dvaereds and I can give you more of a close-up view. Besides, I don't want to get into a fight with you as my only wingmate. I don't trust you. At least if I'm on your ship, I can kill you and steal the ship, eh?" You let out a nervous laugh as he turns and walks toward the direction of the landing bay. You begin to wonder just what he's going to show you that he thinks will put you on the side of the FLF, what exactly they hate so much about House Dvaered. You wanted to avoid picking sides, but you decide that at least there's no risk with seeing his point of view.]])
 response_yes.learn = _([["Good to hear! I'm not used to having someone who actually wants to learn of our struggles, but I'll do my best to enlighten you. I think as good a place to start as any is my homeworld, %s.
 
-"Oh, my name's Flint, by the way. And, uh, this should go without saying, but I still don't fully trust you, and I'm prepared to defend myself if I need to. We FLF soldiers are pretty damn good with a laser gun." He playfully winks. You laugh a little and promise you won't do anything funny. "Good to hear!" he responds with a grin. "I'll be waiting on your ship when you're ready. See you soon!"
+"Oh, my name's Flint, by the way. And, uh, this should go without saying, but I still don't fully trust you, and I'm prepared to defend myself if I need to. We FLF soldiers are pretty damn good with a laser gun." He playfully winks. You laugh a little and promise you won't do anything funny. "Good to hear!" he responds with a grin. "I'll be waiting at the landing bay when you're ready. See you soon!"
 
-Flint turns away, waves, and walks toward the hangar. Despite the threat, you detect that Flint trusts you at least a little, and you let out a slight smile. This tour of the Frontier should prove enlightening indeed.]])
+Flint turns away, waves, and walks toward the landing bay. Despite the threat, you detect that Flint trusts you at least a little, and you let out a slight smile. This tour of the Frontier should prove enlightening indeed.]])
 
-tohelp_homeworld = _([[As you begin docking procedures, you notice Flint looking up at a Dvaered formation nearby. "Damn Dvaereds, acting like they own the place...." You try to be friendly by agreeing that the Dvaereds should stop trying to assert authority over the Frontier and let the Frontier forces handle crime. Flint looks at you like you've grown two heads. "It's not... do you even see what they're doing? Just... just look, why don't you?" You take a closer look at the Dvaered fleet and them chasing a Frontier Lancelot. Flint continues. "They do this shit all the time, attack random Frontier on the pretense of 'fighting terrorism'. Makes me sick." Eventually, as they start to fade from view, the Dvaereds seemingly stop harassing the Frontier ship.
+homeworld_text = {}
+homeworld_text.help = {}
+homeworld_text.help[1] = _([[As you begin to enter the atmosphere, you notice Flint looking up at a Dvaered formation nearby. "Damn Dvaereds, acting like they own the place...." The fleet slowly fades from view as you enter the planet's atmosphere. Flint sighs and looks down at the oceanic world. You say that it's a beautiful world and that it would be a shame if the Dvaereds turned it into a mining colony. Flint looks at you like you've grown two heads.
 
-Flint looks at you and continues. "This is what's wrong with you, %s. You keep making assumptions about our situation that you seem to not even realize you're making. You need to shut up and listen to us." You quickly apologize, and Flint sighs. "Well, this is my homeworld. Beautiful, isn't it?" You nod and take a moment to appreciate the view as you land. It's a water world filled with all kinds of interesting life forms.]])
+"Do you honestly think that's what our struggle is about? Pretty oceans? There's much more at stake here than that. We're people who the Dvaereds are trying to rule over. This is our struggle for our freedom and autonomy, not to protect nature.
 
-tohelp_homeworld_2 = _([[Flint continues. "But it's more than just beautiful. This planet is home to a special algae that can be used as medicine. We've been using it sustainably for generations. Outside pressures always try to get us to harvest more, but we refuse because we know that it will devastate the natural ecosystem these algae and many other organisms depend on.
+"Besides that, this planet isn't just looks, you know. It's home to a special algae that we use as medicine, and it's both vital to the health of people in the entire Frontier and in need of delicate conservation, which we've balanced for hundreds of cycles. The threat of losing our lives because we lose our supply of vital medicine is far greater than any risk of a damn mining operation ruining a view that you apparently think we'd risk our lives for."]])
 
-"The Dvaereds don't care about the ecosystem at all. They would probably drill holes all over the seafloor to mine the scant ore deposits found on the surface. This beautiful world would just be another resource for their war industry."]])
+homeworld_text.help[2] = _([[A brief pause passes, and you apologize. Flint sighs. "This is the problem with imperials like you. Always thinking you know everything when you know absolutely nothing."]])
 
 motive_text = {}
 motive_text.help = _("To help the FLF")
