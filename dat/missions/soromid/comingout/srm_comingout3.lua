dd2a52ecc1d1dd0649c01ae8d422c2fb9e15b0c2
Layla Marchant <diligentcircle@riseup.net>
2021-07-10 20:03:50 -0400

    Touched up comingout3 a bit.

--- dat/missions/soromid/comingout/srm_comingout3.lua
+++ dat/missions/soromid/comingout/srm_comingout3.lua
@@ -43,26 +43,23 @@ require "nextjump"
 require "missions/soromid/common"
 
 
-title = {}
-text = {}
+ask_text = _([[Chelsea gleefully waves you over. "It's nice to see you again!" she says. The two of you chat a bit about her venture into the piloting business; all is well from the sound of it. "Say," she says, "someone offered me a really well-paying mission recently, but I had to decline because my ship isn't really up to task. If you could just escort my ship through that mission I could share the pay with you! Your half would be %s. How about it?"]])
 
-title[1] = _("A Friend's Aid")
-text[1] = _([[Chelsea gleefully waves you over. "It's nice to see you again!" she says. The two of you chat a bit about her venture into the piloting business; all is well from the sound of it. "Say," she says, "someone offered me a really interesting mission recently, but I had to decline because my ship isn't really up to task. If you could just escort my ship through that mission I could share the pay with you! Your half would be %s. How about it?"]])
+start_text = _([["Awesome! I really appreciate it!
 
-text[2] = _([["Awesome! I really appreciate it!
-    "So the mission is in theory a pretty simple one: I need to deliver some cargo to %s in the %s system. Trouble is apparently I'll be getting in the middle of some sort of trade dispute with a shady Soromid company that's bribed the local Soromid pilots. Needless to say we can expect to be attacked by some thugs and the Soromid military isn't likely to be of much help.
-    "That's where you come in. I just need you to follow me along, make sure I finish jumping or landing before you do, and if we encounter any hostilities, help me shoot them down. Shouldn't be too too hard as long as you've got a decent ship. I'll meet you out in space!"]])
+"So the mission is in theory a pretty simple one: I need to deliver some cargo to %s in the %s system. Trouble is apparently I'll be getting in the middle of some sort of trade dispute with a shady company that's bribed the local officials. Needless to say we can expect to be attacked by some thugs and probably won't get any external help.
 
-text[3] = _([["Ah, OK. Let me know if you change your mind."]])
+"That's where you come in. I just need you to follow me along, make sure I finish jumping or landing before you do, and if we encounter any hostilities, help me shoot them down. Shouldn't be too too hard as long as you've got a decent ship. I'll meet you out in space!"]])
 
-text[4] = _([["I could still use your help with that mission! Could you help me out?"]])
+no_text = _([["Ah, OK. Let me know if you change your mind."]])
 
-title[5] = _("Mission Failed")
-text[5] = _("You have lost contact with Chelsea and therefore failed the mission.")
+ask_again_text = _([["I could still use your help with that mission! Could you help me out?"]])
 
-title[6] = _("Another Happy Landing")
-text[6] = _([[You successfully land and dock alongside Chelsea and she approaches the worker for the cargo delivery. The worker gives her a weird look, but collects the cargo with the help of some robotic drones and hands her a credit chip. When you get back to your ships, Chelsea transfers the sum of %s to your account, and you idly chat with her for a while.
-    "Anyway, I should probably get going now," she says. "But I really appreciated the help there! Get in touch with me again sometime. We make a great team!" You agree, and you both go your separate ways once again.]])
+left_fail_text = _("You have lost contact with Chelsea and therefore failed the mission.")
+
+success_text = _([[You successfully land and dock alongside Chelsea and she approaches the worker for the cargo delivery. The worker gives her a weird look, but collects the cargo with the help of some robotic drones and hands her a credit chip. When you get back to your ships, Chelsea transfers the sum of %s to your account, and you idly chat with her for a while.
+
+"Anyway, I should probably get going now," she says. "But I really appreciated the help there! Get in touch with me again sometime. We make a great team!" You agree, and you both go your separate ways once again.]])
 
 misn_title = _("A Friend's Aid")
 misn_desc = _("Chelsea needs you to escort her to %s.")
@@ -91,40 +88,40 @@ function create ()
    credits = 500000
    started = false
 
-   misn.setNPC( npc_name, "soromid/unique/chelsea.webp", npc_desc )
+   misn.setNPC(npc_name, "soromid/unique/chelsea.webp", npc_desc)
 end
 
 
 function accept ()
    local txt
    if started then
-      txt = text[4]
+      txt = ask_again_text
    else
-      txt = text[1]:format( creditstring( credits ) )
+      txt = ask_text:format(creditstring(credits))
    end
    started = true
 
-   if tk.yesno( title[1], txt ) then
-      tk.msg( title[1], text[2]:format( misplanet:name(), missys:name() ) )
+   if tk.yesno("", txt) then
+      tk.msg("", start_text:format(misplanet:name(), missys:name()))
 
       misn.accept()
 
-      misn.setTitle( misn_title )
-      misn.setDesc( misn_desc:format( misplanet:name() ) )
-      misn.setReward( creditstring( credits ) )
-      marker = misn.markerAdd( missys, "low" )
+      misn.setTitle(misn_title)
+      misn.setDesc(misn_desc:format(misplanet:name()))
+      misn.setReward(creditstring(credits))
+      marker = misn.markerAdd(missys, "low")
 
-      osd_desc[1] = osd_desc[1]:format( misplanet:name(), missys:name() )
-      misn.osdCreate( misn_title, osd_desc )
+      osd_desc[1] = osd_desc[1]:format( misplanet:name(), missys:name())
+      misn.osdCreate(misn_title, osd_desc)
 
       startplanet = planet.cur()
 
-      hook.takeoff( "takeoff" )
-      hook.jumpout( "jumpout" )
-      hook.jumpin( "jumpin" )
-      hook.land( "land" )
+      hook.takeoff("takeoff")
+      hook.jumpout("jumpout")
+      hook.jumpin("jumpin")
+      hook.land("land")
    else
-      tk.msg( title[1], text[3] )
+      tk.msg("", no_text)
       misn.finish()
    end
 end
@@ -136,48 +133,50 @@ end
 
 
 function spawnChelseaShip( param )
-   chelsea = pilot.add( "Llama", "Comingout_associates", param, _("Chelsea") )
-   chelsea:rmOutfit( "all" )
-   chelsea:rmOutfit( "cores" )
-   chelsea:addOutfit( "Unicorp PT-80 Core System" )
-   chelsea:addOutfit( "Unicorp Hawk 300 Engine" )
-   chelsea:addOutfit( "Unicorp D-4 Light Plating" )
-   chelsea:addOutfit( "Laser Turret MK1", 2 )
-   chelsea:addOutfit( "Small Shield Booster" )
-   chelsea:addOutfit( "Improved Refrigeration Cycle" )
-   chelsea:addOutfit( "Cargo Pod", 2 )
+   chelsea = pilot.add("Llama", "Comingout_associates", param, _("Chelsea"))
+   chelsea:rmOutfit("all")
+   chelsea:rmOutfit("cores")
+   chelsea:addOutfit("Unicorp PT-80 Core System")
+   chelsea:addOutfit("Unicorp Hawk 300 Engine")
+   chelsea:addOutfit("Unicorp D-4 Light Plating")
+   chelsea:addOutfit("Plasma Turret MK1", 2)
+   chelsea:addOutfit("Small Shield Booster")
+   chelsea:addOutfit("Improved Refrigeration Cycle")
+   chelsea:addOutfit("Cargo Pod", 2)
 
-   chelsea:setHealth( 100, 100 )
-   chelsea:setEnergy( 100 )
-   chelsea:setTemp( 0 )
-   chelsea:setFuel( true )
+   chelsea:setHealth(100, 100)
+   chelsea:setEnergy(100)
+   chelsea:setTemp(0)
+   chelsea:setFuel(true)
 
-   chelsea:cargoAdd( "Industrial Goods", chelsea:cargoFree() )
+   chelsea:cargoAdd("Industrial Goods", chelsea:cargoFree())
 
    chelsea:setFriendly()
    chelsea:setHilight()
    chelsea:setVisible()
    chelsea:setInvincPlayer()
 
-   hook.pilot( chelsea, "death", "chelsea_death" )
-   hook.pilot( chelsea, "jump", "chelsea_jump" )
-   hook.pilot( chelsea, "land", "chelsea_land" )
-   hook.pilot( chelsea, "attacked", "chelsea_attacked" )
+   hook.pilot(chelsea, "death", "chelsea_death")
+   hook.pilot(chelsea, "jump", "chelsea_jump")
+   hook.pilot(chelsea, "land", "chelsea_land")
+   hook.pilot(chelsea, "attacked", "chelsea_attacked")
 
    chelsea_jumped = false
 end
 
 
 function spawnThug( param )
-   local shiptypes = { "Hyena", "Hyena", "Hyena", "Shark", "Lancelot" }
-   local shiptype = shiptypes[ rnd.rnd( 1, #shiptypes ) ]
-   thug = pilot.add( shiptype, "Comingout_thugs", param, _("Thug %s"):format( _(shiptype) ) )
+   local f = faction.dynAdd(nil, N_("Thug"), nil, "baddie")
+   local shiptypes = {"Hyena", "Hyena", "Hyena", "Shark", "Lancelot"}
+   local shiptype = shiptypes[rnd.rnd(1, #shiptypes)]
+
+   thug = pilot.add(shiptype, f, param, _("Thug %s"):format(_(shiptype)))
 
    thug:setHostile()
 
-   hook.pilot( thug, "death", "thug_removed" )
-   hook.pilot( thug, "jump", "thug_removed" )
-   hook.pilot( thug, "land", "thug_removed" )
+   hook.pilot(thug, "death", "thug_removed")
+   hook.pilot(thug, "jump", "thug_removed")
+   hook.pilot(thug, "land", "thug_removed")
 end
 
 
@@ -186,16 +185,16 @@ function jumpNext ()
       chelsea:taskClear()
       chelsea:control()
       if system.cur() == missys then
-         chelsea:land( misplanet, true )
+         chelsea:land(misplanet, true)
       else
-         chelsea:hyperspace( getNextSystem( system.cur(), missys ), true )
+         chelsea:hyperspace(getNextSystem(system.cur(), missys), true)
       end
    end
 end
 
 
 function takeoff ()
-   spawnChelseaShip( startplanet )
+   spawnChelseaShip(startplanet)
    jumpNext()
    spawnThug()
 end
@@ -207,25 +206,25 @@ end
 
 
 function jumpin ()
-   if chelsea_jumped and system.cur() == getNextSystem( lastsys, missys ) then
-      spawnChelseaShip( lastsys )
+   if chelsea_jumped and system.cur() == getNextSystem(lastsys, missys) then
+      spawnChelseaShip(lastsys)
       jumpNext()
-      hook.timer( 5000, "thug_timer" )
+      hook.timer(5000, "thug_timer")
    else
-      fail( plflee_msg )
+      fail(plflee_msg)
    end
 end
 
 
 function land ()
    if planet.cur() == misplanet then
-      tk.msg( title[6], text[6]:format( creditstring( credits ) ) )
-      player.pay( credits )
-      srm_addComingOutLog( log_text )
-      misn.finish( true )
+      tk.msg("", success_text:format(creditstring(credits)))
+      player.pay(credits)
+      srm_addComingOutLog(log_text)
+      misn.finish(true)
    else
-      tk.msg( title[5], text[5] )
-      misn.finish( false )
+      tk.msg("", left_fail_text)
+      misn.finish(false)
    end
 end
 
@@ -233,41 +232,41 @@ end
 function thug_timer ()
    spawnThug()
    if system.cur() == missys then
-      spawnThug( lastsys )
+      spawnThug(lastsys)
    end
 end
 
 
 function chelsea_death ()
-   fail( chelkill_msg )
+   fail(chelkill_msg)
 end
 
 
 function chelsea_jump( p, jump_point )
-   if jump_point:dest() == getNextSystem( system.cur(), missys ) then
-      player.msg( cheljump_msg:format( jump_point:dest():name() ) )
+   if jump_point:dest() == getNextSystem(system.cur(), missys) then
+      player.msg(cheljump_msg:format(jump_point:dest():name()))
       chelsea_jumped = true
    else
-      fail( chelflee_msg )
+      fail(chelflee_msg)
    end
 end
 
 
 function chelsea_land( p, planet )
    if planet == misplanet then
-      player.msg( chelland_msg:format( planet:name() ) )
+      player.msg(chelland_msg:format(planet:name()))
       chelsea_jumped = true
    else
-      fail( chelflee_msg )
+      fail(chelflee_msg)
    end
 end
 
 
 function chelsea_attacked ()
    if chelsea ~= nil and chelsea:exists() then
-      chelsea:control( false )
-      if distress_timer_hook ~= nil then hook.rm( distress_timer_hook ) end
-      distress_timer_hook = hook.timer( 1000, "chelsea_distress_timer" )
+      chelsea:control(false)
+      if distress_timer_hook ~= nil then hook.rm(distress_timer_hook) end
+      distress_timer_hook = hook.timer(1000, "chelsea_distress_timer")
    end
 end
 
@@ -289,11 +288,11 @@ function fail( message )
    if message ~= nil then
       -- Pre-colourized, do nothing.
       if message:find("#") then
-         player.msg( message )
+         player.msg(message)
       -- Colourize in red.
       else
-         player.msg( "#r" .. message .. "#0" )
+         player.msg("#r" .. message .. "#0")
       end
    end
-   misn.finish( false )
+   misn.finish(false)
 end
