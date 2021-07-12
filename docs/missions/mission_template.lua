bcbe62861b9657e0751c690d98b8ef3725070b3e
Layla Marchant <diligentcircle@riseup.net>
2021-07-12 12:20:25 -0400

    Cleaned up and fixed the mission template.

--- docs/missions/mission_template.lua
+++ docs/missions/mission_template.lua
@@ -58,19 +58,18 @@ require "numstring"
 require "missions/neutral/common"
 
 --[[
-Multi-paragraph dialog strings should go here, each with an identifiable
-name. You can see here that we wrap strings that are displayed to the
-player with `_()`. This is a call to gettext, which enables
-localization. The _() call should be used directly on the string, as
-shown here, instead of on a variable, so that the script which figures
-out what all the translatable text is can find it.
+Multi-paragraph or long dialog strings should go here, each with an
+identifiable name. You can see here that we wrap strings that are
+displayed to the player with `_()`. This is a call to gettext, which
+enables localization. The _() call should be used directly on the
+string, as shown here, instead of on a variable, so that the script
+which figures out what all the translatable text is can find it.
 
 When writing dialog, write it like a book (in the present-tense), with
-paragraphs and quotations and all that good stuff. Leave the first
-paragraph unindented, and indent every subsequent paragraph by four (4)
-spaces. Use quotation marks as would be standard in a book. However, do
-*not* quote the player speaking; instead, paraphrase what the player
-generally says, as shown below.
+paragraphs and quotations and all that good stuff. Use a double line
+break, as shown below, for new paragraphs. Use quotation marks as would
+be standard in a book. However, do *not* quote the player speaking;
+instead, paraphrase what the player generally says, as shown below.
 
 In most cases, you should use double-brackets for your multi-paragraph
 dialog strings, as shown below.
@@ -84,11 +83,21 @@ are portions that will be filled in later by the mission via the
 `string.format()` function.
 --]]
 ask_text = _([[As you approach the guy, he looks up in curiosity. You sit down and ask him how his day is. "Why, fine," he answers. "How are you?" You answer that you are fine as well and compliment him on his suit, which seems to make his eyes light up. "Why, thanks! It's my favorite suit! I had it custom tailored, you know.
-    "Actually, that reminds me! There was a special suit on %s in the %s system, the last one I need to complete my collection, but I don't have a ship. You do have a ship, don't you? So I'll tell you what, give me a ride and I'll pay you %s for it! What do you say?"]])
+
+"Actually, that reminds me! There was a special suit on %s in the %s system, the last one I need to complete my collection, but I don't have a ship. You do have a ship, don't you? So I'll tell you what, give me a ride and I'll pay you %s for it! What do you say?"]])
+
+ask_again_text = _([["Ah, it's you again! Have you changed your mind? Like I said, I just need transport to %s in the %s system, and I'll pay you %s when we get there. How's that sound?"]])
+
+accept_text = _([["Fantastic! I knew you would do it! Like I said, I'll pay you as soon as we get there. No rush! Just bring me there when you're ready.]])
+
+finish_text = _([[As you arrive on %s, your passenger reacts with glee. "I must sincerely thank you, kind stranger! Now I can finally complete my suit collection, and it's all thanks to you. Here is %s, as we agreed. I hope you have safe travels!"]])
+
+misn_desc = _("A well-dressed man wants you to take him to %s in the %s system so he get some sort of special suit.")
+misn_log = _([[You helped transport a well-dressed man to %s so that he could buy some kind of special suit to complete his collection.]])
 
 
 --[[ 
-First you need to *create* the mission.  This is *obligatory*.
+First you need to *create* the mission. This is *obligatory*.
 
 You have to set the NPC and the description. These will show up at the
 bar with the character that gives the mission and the character's
@@ -103,20 +112,26 @@ function create ()
    -- Here we use the `creditstring()` function to convert our credits
    -- from a number to a string. This function both applies gettext
    -- correctly for variable amounts (by using the ngettext function),
-   -- and formats the number in a way that is appropriate for Naev (by
-   -- using the numstring function). You should always use this when
-   -- displaying a number of credits.
+   -- and formats the number in a way that is appropriate for Naikari
+   -- (by using the numstring function). You should always use this when
+   -- displaying a number of credits. The exception is if you're
+   -- displaying the amount of credits the player has, in which case the
+   -- second return value of `player.credits()` should be used.
    reward_text = creditstring(credits)
    
    -- If we needed to claim a system, we would do that here with
    -- something like the following commented out statement. However,
-   -- this mission won't be doing anything fancy with the system, so we
-   -- won't make a system claim for it.
+   -- this mission won't be doing anything with the system itself (it
+   -- only wants us to land somewhere), so we won't make a system claim
+   -- for it.
    --if not misn.claim(missys) then misn.finish(false) end
 
    -- Give the name of the NPC and the portrait used. You can see all
-   -- available portraits in dat/gfx/portraits.
-   misn.setNPC( _("A well-dressed man"), "neutral/unique/youngbusinessman.png", _("This guy is wearing a nice suit.") )
+   -- available portraits in artwork/gfx/portraits.
+   misn.setNPC(
+         _("A well-dressed man"),
+         "neutral/unique/youngbusinessman.webp",
+         _("This guy is wearing a nice suit."))
 end
 
 
@@ -136,7 +151,7 @@ function accept ()
       -- We use `string.format()` here to fill in the destination and
       -- reward text. `s1:format(s2)` is a shorthand for
       -- `string.format(s1, s2)`.
-      text = _([["Ah, it's you again! Have you changed your mind? Like I said, I just need transport to %s in the %s system, and I'll pay you %s when we get there. How's that sound?"]]):format( misplanet:name(), missys:name(), reward_text )
+      text = ask_again_text
    else
       text = ask_text
       talked = true
@@ -144,10 +159,10 @@ function accept ()
 
    -- This will create the typical "Yes/No" dialogue. It returns true if
    -- yes was selected. 
-   if tk.yesno( _("My Suit Collection"),
-         ask_text:format( reward_text ) ) then
+   if tk.yesno("", text:format(
+            misplanet:name(), missys:name(), reward_text)) then
       -- Followup text.
-      tk.msg( _("My Suit Collection"), _([["Fantastic! I knew you would do it! Like I said, I'll pay you as soon as we get there. No rush! Just bring me there when you're ready.]]) )
+      tk.msg("", accept_text)
 
       -- Accept the mission
       misn.accept()
@@ -155,18 +170,18 @@ function accept ()
       -- Mission details:
       -- You should always set mission details right after accepting the
       -- mission.
-      misn.setTitle( _("Suits Me Fine") )
-      misn.setReward( reward_text )
-      misn.setDesc( _("A well-dressed man wants you to take him to %s in the %s system so he get some sort of special suit."):format( misplanet:name(), missys:name() ) )
+      misn.setTitle(_("The Special Suit"))
+      misn.setReward(reward_text)
+      misn.setDesc(misn_desc:format(misplanet:name(), missys:name()))
 
       -- Markers indicate a target system on the map, it may not be
       -- needed depending on the type of mission you're writing.
-      misn.markerAdd( missys, "low" )
+      misn.markerAdd(missys, "low")
 
       -- The OSD shows your objectives.
       local osd_desc = {}
-      osd_desc[1] = _("Fly to %s in the %s system"):format( misplanet:name(), missys:name() )
-      misn.osdCreate( _("Suits Me Fine"), osd_desc )
+      osd_desc[1] = _("Fly to %s in the %s system"):format(misplanet:name(), missys:name())
+      misn.osdCreate(_("Suits Me Fine"), osd_desc)
 
       -- This is where we would define any other variables we need, but
       -- we won't need any for this example.
@@ -174,7 +189,7 @@ function accept ()
       -- Hooks go here. We use hooks to cause something to happen in
       -- response to an event. In this case, we use a hook for when the
       -- player lands on a planet.
-      hook.land( "land" )
+      hook.land("land")
    else
       -- Call misn.finish() to end the conversation with the NPC without
       -- getting rid of him.
@@ -183,7 +198,7 @@ function accept ()
 end
 
 
--- This is our land hook function. Once `hook.land( "land" )` is called,
+-- This is our land hook function. Once `hook.land("land")` is called,
 -- this function will be called any time the player lands.
 function land ()
    -- First check to see if we're on our target planet.
@@ -191,18 +206,18 @@ function land ()
       -- Mission accomplished! Now we do an outro dialog and reward the
       -- player. Rewards are usually credits, as shown here, but
       -- other rewards can also be given depending on the circumstances.
-      tk.msg( _([[As you arrive on %s, your passenger reacts with glee. "I must sincerely thank you, kind stranger! Now I can finally complete my suit collection, and it's all thanks to you. Here is %s, as we agreed. I hope you have safe travels!"]]):format( misplanet:name(), reward_text )
+      tk.msg("", finish_text:format(misplanet:name(), reward_text)
 
       -- Reward the player. Rewards are usually credits, as shown here,
       -- but other rewards can also be given depending on the
       -- circumstances.
-      player.pay( credits )
+      player.pay(credits)
 
       -- Add a log entry. This should only be done for unique missions.
-      addMiscLog( _([[You helped transport a well-dressed man to %s so that he could buy some kind of special suit to complete his collection.]]):format( misplanet:name() ) )
+      addMiscLog(misn_log:format(misplanet:name()))
       
       -- Finish the mission. Passing the `true` argument marks the
       -- mission as complete.
-      misn.finish( true )
+      misn.finish(true)
    end
 end
