f6f17c6e08577af6f6c433196020b55d609c2b9a
Layla Marchant <diligentcircle@riseup.net>
2021-07-18 21:15:54 -0400

    Fixed up comingout5.

--- dat/missions/soromid/comingout/srm_comingout5.lua
+++ dat/missions/soromid/comingout/srm_comingout5.lua
@@ -40,21 +40,19 @@ require "missions/soromid/comingout/srm_comingout3"
 require "missions/soromid/common"
 
 
-title = {}
 text = {}
 
-title[1] = _("The Dirty Job")
-text[1] = _([[You walk over to Chelsea to greet them when you notice an unpleasant odor coming off of them. Chelsea notices you. "Ah! %s! Uh, sorry about the smell. I don't know why the hell I did this, but I took a job from some guy here and now I'm stuck with it." You ask what kind of job it is. "Erm, I kind of agreed to take their trash from them." You grimace. "Yeah," Chelsea says, "it's gross. And what's worse, I'm in over my head. I've already taken the garbage and my new ship is packed to the brim with the stuff, but there's thugs outside that seem to be waiting for me." A look of rage appears on their face as they turn to the side. "You can probably guess who's responsible for that." The thought had crossed your mind as well.
-    Chelsea turns back to you. "I know I ask a lot of you, but could you help me once again? I just need an escort to %s so I can drop off this garbage there. I'll give you %s for the trouble. What do you say?"]])
+ask_text = _([[You walk over to Chelsea to greet them when you notice an unpleasant odor coming off of them. Chelsea notices you. "Ah! %s! Uh, sorry about the smell. I don't know why the hell I did this, but I took a job from some guy here and now I'm stuck with it." You ask what kind of job it is. "Erm, I kind of agreed to take their trash from them." You grimace. "Yeah," Chelsea says, "it's gross. And what's worse, I'm in over my head. I've already taken the garbage and my new ship is packed to the brim with the stuff, but there's thugs outside giving me a lot of trouble.
 
-text[2] = _([["I appreciate it very much. I'll wait at the hangar until you're ready to take off. Get ready for a fight when we get out of the atmosphere; it's going to be a bumpy ride."]])
+"I, uh, know I ask a lot of you, but could you help me out again? I just need an escort to %s so I can drop off this garbage there. I'll give you %s for the trouble. What do you say?"]])
 
-text[3] = _([["OK, I understand. I guess I'll have to find some other way to get rid of all this garbage..."]])
+start_text = _([["I appreciate it very much. I'll wait at the spaceport until you're ready to take off. Get ready for a fight when we get out of the atmosphere; it's going to be a bumpy ride."]])
 
-text[4] = _([["I'm not having any luck coming up with a plan to get rid of all of this garbage without getting jumped by those thugs. Is there any chance you could reconsider being my escort? It would be a big help."]])
+no_text = _([["OK, I understand. I guess I'll have to find some other way to get rid of all this garbage..."]])
 
-title[6] = _("The Unbearable Smell Now Ends")
-text[6] = _([[As you dock, you can't help but notice the foul smell of garbage all around you. The planet really does fit the name. You grimace as you watch workers unload what must be hundreds of tonnes of garbage from Chelsea's ship, some of which is leaking. Eventually Chelsea's ship is emptied and you and Chelsea are handed your credit chips for the job. You and Chelsea part ways, vowing to take a shower immediately while Chelsea vows to scrub the cargo hold of their ship clean.]])
+ask_again_text = _([["I'm not having any luck coming up with a plan to get rid of all of this garbage without getting jumped by those thugs. Is there any chance you could reconsider escorting me? It would be a big help."]])
+
+success_text = _([[As you dock, you can't help but notice the foul smell of garbage all around you. The planet really does fit the name. You grimace as you watch workers unload what must be hundreds of tonnes of garbage from Chelsea's ship, some of which is leaking. Eventually Chelsea's ship is emptied and you and Chelsea are handed your credit chips for the job. You and Chelsea part ways, with you vowing to take a shower immediately while Chelsea vows to scrub the cargo hold of their ship clean.]])
 
 misn_title = _("Waste Collector")
 misn_desc = _("Chelsea needs an escort to %s so they can get rid of the garbage now filling their ship.")
@@ -66,104 +64,114 @@ log_text = _([[You helped Chelsea get rid of a load of garbage they naively agre
 
 
 function create ()
-   misplanet, missys = planet.get( "The Stinker" )
+   misplanet, missys = planet.get("The Stinker")
    if misplanet == nil or missys == nil or system.cur():jumpDist(missys) > 4 then
-      misn.finish( false )
+      misn.finish(false)
    end
 
    credits = 500000
    started = false
 
-   misn.setNPC( npc_name, "soromid/unique/chelsea.png", npc_desc )
+   misn.setNPC(npc_name, "soromid/unique/chelsea.png", npc_desc)
 end
 
 
 function accept ()
    local txt
    if started then
-      txt = text[4]
+      txt = ask_again_text
    else
-      txt = text[1]:format( player.name(), misplanet:name(), creditstring( credits ) )
+      txt = ask_text:format(player.name(), misplanet:name(), creditstring(credits))
    end
    started = true
 
-   if tk.yesno( title[1], txt ) then
-      tk.msg( title[1], text[2] )
+   if tk.yesno("", txt) then
+      tk.msg("", start_text)
 
       misn.accept()
 
-      misn.setTitle( misn_title )
-      misn.setDesc( misn_desc:format( misplanet:name() ) )
-      misn.setReward( creditstring( credits ) )
-      marker = misn.markerAdd( missys, "low" )
+      misn.setTitle(misn_title)
+      misn.setDesc(misn_desc:format(misplanet:name()))
+      misn.setReward(creditstring(credits))
+      marker = misn.markerAdd(missys, "low")
 
-      osd_desc[1] = osd_desc[1]:format( misplanet:name(), missys:name() )
-      misn.osdCreate( misn_title, osd_desc )
+      local nextsys = getNextSystem(system.cur(), missys)
+      local jumps = system.cur():jumpDist(missys)
+      local osd_desc = {}
+      osd_desc[1] = string.format(
+            _("Protect Chelsea and wait for her to jump to %s"),
+            nextsys:name())
+      osd_desc[2] = string.format(_("Jump to %s"), nextsys:name())
+      osd_desc[3] = string.format(
+            _("%s more jumps after this one"), numstring(jumps - 1))
+      misn.osdCreate(misn_title, osd_desc)
 
       startplanet = planet.cur()
 
-      hook.takeoff( "takeoff" )
-      hook.jumpout( "jumpout" )
-      hook.jumpin( "jumpin" )
-      hook.land( "land" )
+      hook.takeoff("takeoff")
+      hook.jumpout("jumpout")
+      hook.jumpin("jumpin")
+      hook.land("land")
    else
-      tk.msg( title[1], text[3] )
+      tk.msg("", no_text)
       misn.finish()
    end
 end
 
 
-function spawnChelseaShip( param )
-   chelsea = pilot.add( "Rhino", "Comingout_associates", param, _("Chelsea") )
-   chelsea:rmOutfit( "all" )
-   chelsea:rmOutfit( "cores" )
-   chelsea:addOutfit( "Unicorp PT-400 Core System" )
-   chelsea:addOutfit( "Melendez Buffalo XL Engine" )
-   chelsea:addOutfit( "S&K Medium Cargo Hull" )
-   chelsea:addOutfit( "Heavy Ripper Turret", 2 )
-   chelsea:addOutfit( "Enygma Systems Turreted Fury Launcher", 2 )
-   chelsea:addOutfit( "Fury Missile", 80 )
-   chelsea:addOutfit( "Medium Shield Booster" )
-   chelsea:addOutfit( "Targeting Array" )
-   chelsea:addOutfit( "Droid Repair Crew" )
-   chelsea:addOutfit( "Medium Cargo Pod", 4 )
+function spawnChelseaShip(param)
+   chelsea = pilot.add("Rhino", "Comingout_associates", param, _("Chelsea"))
+   chelsea:rmOutfit("all")
+   chelsea:rmOutfit("cores")
+   chelsea:addOutfit("Milspec Aegis 5401 Core System")
+   chelsea:addOutfit("Melendez Buffalo XL Engine")
+   chelsea:addOutfit("S&K Medium Cargo Hull")
+   chelsea:addOutfit("Heavy Ripper Turret", 2)
+   chelsea:addOutfit("Enygma Systems Turreted Fury Launcher", 2)
+   chelsea:addOutfit("Medium Shield Booster")
+   chelsea:addOutfit("Targeting Array")
+   chelsea:addOutfit("Droid Repair Crew")
+   chelsea:addOutfit("Medium Cargo Pod", 4)
 
-   chelsea:setHealth( 100, 100 )
-   chelsea:setEnergy( 100 )
-   chelsea:setTemp( 0 )
-   chelsea:setFuel( true )
+   chelsea:fillAmmo()
+   chelsea:setHealth(100, 100)
+   chelsea:setEnergy(100)
+   chelsea:setTemp(0)
+   chelsea:setFuel(true)
 
-   chelsea:cargoAdd( "Waste Containers", chelsea:cargoFree() )
+   chelsea:cargoAdd("Waste Containers", chelsea:cargoFree())
 
    chelsea:setFriendly()
    chelsea:setHilight()
    chelsea:setVisible()
    chelsea:setInvincPlayer()
 
-   hook.pilot( chelsea, "death", "chelsea_death" )
-   hook.pilot( chelsea, "jump", "chelsea_jump" )
-   hook.pilot( chelsea, "land", "chelsea_land" )
-   hook.pilot( chelsea, "attacked", "chelsea_attacked" )
+   hook.pilot(chelsea, "death", "chelsea_death")
+   hook.pilot(chelsea, "jump", "chelsea_jump")
+   hook.pilot(chelsea, "land", "chelsea_land")
+   hook.pilot(chelsea, "attacked", "chelsea_attacked")
 
    chelsea_jumped = false
 end
 
 
-function spawnThug( param )
+function spawnThug(param)
    local shiptypes = { "Hyena", "Hyena", "Shark", "Lancelot", "Admonisher" }
-   local shiptype = shiptypes[ rnd.rnd( 1, #shiptypes ) ]
-   thug = pilot.add( shiptype, "Comingout_thugs", param, _("Thug %s"):format( _(shiptype) ) )
+   local shiptype = shiptypes[rnd.rnd(1, #shiptypes)]
+
+   thug = pilot.add(shiptype, "Comingout_thugs", param,
+         _("Thug %s"):format(_(shiptype)))
 
    thug:setHostile()
 
-   hook.pilot( thug, "death", "thug_removed" )
-   hook.pilot( thug, "jump", "thug_removed" )
-   hook.pilot( thug, "land", "thug_removed" )
+   hook.pilot(thug, "death", "thug_removed")
+   hook.pilot(thug, "jump", "thug_removed")
+   hook.pilot(thug, "land", "thug_removed")
 end
 
 
 function takeoff ()
-   spawnChelseaShip( startplanet )
+   spawnChelseaShip(startplanet)
    jumpNext()
    spawnThug()
    spawnThug()
@@ -171,19 +179,19 @@ end
 
 
 function land ()
-   if planet.cur() == misplanet then
-      tk.msg( title[6], text[6]:format( numstring( credits ) ) )
-      player.pay( credits )
+   if chelsea_jumped and planet.cur() == misplanet then
+      tk.msg("", success_text:format(numstring(credits)))
+      player.pay(credits)
 
       local t = time.get():tonumber()
-      var.push( "comingout_time", t )
+      var.push("comingout_time", t)
 
-      srm_addComingOutLog( log_text )
+      srm_addComingOutLog(log_text)
 
-      misn.finish( true )
+      misn.finish(true)
    else
-      tk.msg( title[5], text[5] )
-      misn.finish( false )
+      tk.msg("", left_fail_text)
+      misn.finish(false)
    end
 end
 
@@ -192,6 +200,6 @@ function thug_timer ()
    spawnThug()
    spawnThug()
    if system.cur() == missys then
-      spawnThug( lastsys )
+      spawnThug(lastsys)
    end
 end
