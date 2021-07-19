dbea3019624155ca59e3d89b2c3a29d979cbcdea
Layla Marchant <diligentcircle@riseup.net>
2021-07-18 21:32:38 -0400

    Designated Vaal as an authorized waste dump site.

--- dat/missions/neutral/wastedump.lua
+++ dat/missions/neutral/wastedump.lua
@@ -54,7 +54,7 @@ abort_text[1] = _("Sick and tired of smelling garbage, you illegally jettison th
 abort_text[2] = _("You decide that the nearest waste dump location is too far away for you to bother to go to and simply jettison the containers of waste. You hope you don't get caught.")
 abort_text[3] = _("You dump the waste containers into space illegally, noting that you should make sure not to get caught by authorities.")
 
-abort_landed_text = _("In your desperation to rid yourself of the garbage, you clumsily eject it from your cargo pod while you are still landed. Garbage spills all over the hangar and local officials immediately take notice. After you apologize profusely and explain the situation away as an accident, the officials let you off with a fine of %s.")
+abort_landed_text = _("In your desperation to rid yourself of the garbage, you clumsily eject it from your cargo pod while you are still landed. Garbage spills all over the spaceport and local officials immediately take notice. After you apologize profusely and explain the situation away as an accident, the officials let you off with a fine of %s.")
 
 noland_msg = _("Get lost, waste dumping scum! We don't want you here!")
 
@@ -67,14 +67,14 @@ osd_msg = {}
 osd_msg[1] = _("Land on any garbage collection facility (indicated on your map) to drop off the Waste Containers")
 
 -- List of possible waste dump planets.
-dest_planets = { "The Stinker", "Eiroik" }
+dest_planets = {"The Stinker", "Eiroik", "Vaal"}
 
 
 function create ()
    local dist = nil
    local p, sys
-   for i, j in ipairs( dest_planets ) do
-      p, sys = planet.get( j )
+   for i, j in ipairs(dest_planets) do
+      p, sys = planet.get(j)
       if dist == nil or system.cur():jumpDist(sys) < dist then
          dist = system.cur():jumpDist(sys)
       end
@@ -87,16 +87,16 @@ function create ()
 
    landed = true
 
-   for i, j in ipairs( dest_planets ) do
+   for i, j in ipairs(dest_planets) do
       local p, sys
-      p, sys = planet.get( j )
-      misn.markerAdd( sys, "computer" )
+      p, sys = planet.get(j)
+      misn.markerAdd(sys, "computer")
    end
 
    -- Set mission details
-   misn.setTitle( misn_title )
-   misn.setDesc( misn_desc )
-   misn.setReward( misn_reward:format( creditstring( credits_factor ) ) )
+   misn.setTitle(misn_title)
+   misn.setDesc(misn_desc)
+   misn.setReward(misn_reward:format(creditstring(credits_factor)))
 end
 
 
@@ -106,17 +106,17 @@ function accept ()
    local q = player.pilot():cargoFree()
    credits = credits_factor * q + credits_mod
 
-   local txt = text[ rnd.rnd( 1, #text ) ]
-   tk.msg( "", txt:format( creditstring( credits ) ) )
+   local txt = text[ rnd.rnd(1, #text) ]
+   tk.msg("", txt:format(creditstring(credits)))
 
-   local c = misn.cargoNew( N_("Waste Containers"), N_("A bunch of waste containers leaking all sorts of indescribable liquids.") )
-   cid = misn.cargoAdd( c, q )
-   player.pay( credits )
+   local c = misn.cargoNew(N_("Waste Containers"), N_("A bunch of waste containers leaking all sorts of indescribable liquids."))
+   cid = misn.cargoAdd(c, q)
+   player.pay(credits)
 
-   misn.osdCreate( osd_title, osd_msg )
+   misn.osdCreate(osd_title, osd_msg)
 
-   hook.takeoff( "takeoff" )
-   hook.land( "land" )
+   hook.takeoff("takeoff")
+   hook.land("land")
 end
 
 
@@ -128,11 +128,11 @@ end
 function land ()
    landed = true
 
-   for i, j in ipairs( dest_planets ) do
-      if planet.get( j ) == planet.cur() then
-         local txt = finish_text[ rnd.rnd( 1, #finish_text ) ]
-         tk.msg( "", txt )
-         misn.finish( true )
+   for i, j in ipairs(dest_planets) do
+      if planet.get(j) == planet.cur() then
+         local txt = finish_text[ rnd.rnd(1, #finish_text) ]
+         tk.msg("", txt)
+         misn.finish(true)
       end
    end
 end
@@ -140,19 +140,19 @@ end
 
 function abort ()
    if landed then
-      misn.cargoRm( cid )
+      misn.cargoRm(cid)
       local fine = 2 * credits
-      tk.msg( "", abort_landed_text:format( creditstring( fine ) ) )
-      player.pay( -fine )
-      misn.finish( false )
+      tk.msg("", abort_landed_text:format(creditstring(fine)))
+      player.pay(-fine)
+      misn.finish(false)
    else
-      local txt = abort_text[ rnd.rnd( 1, #abort_text ) ]
-      tk.msg( "", txt )
+      local txt = abort_text[ rnd.rnd(1, #abort_text) ]
+      tk.msg("", txt)
 
-      misn.cargoJet( cid )
+      misn.cargoJet(cid)
 
       -- Make everyone angry
-      for i, j in ipairs( pilot.get() ) do
+      for i, j in ipairs(pilot.get()) do
          j:setHostile()
       end
 
@@ -161,7 +161,7 @@ function abort ()
       local f = nil
       if presences then
          local strongest_amount = 0
-         for k, v in pairs( presences ) do
+         for k, v in pairs(presences) do
             if v > strongest_amount then
                f = faction.get(k)
                strongest_amount = v
@@ -170,46 +170,46 @@ function abort ()
       end
 
       local choices
-      if f == faction.get( "Empire" ) then
+      if f == faction.get("Empire") then
          choices = { "Empire Sml Defense", "Empire Lge Attack", "Empire Med Attack" }
-      elseif f == faction.get( "Goddard" ) then
+      elseif f == faction.get("Goddard") then
          choices = { "Goddard Goddard", "Goddard Lancelot" }
-      elseif f == faction.get( "Dvaered" ) then
+      elseif f == faction.get("Dvaered") then
          choices = { "Dvaered Big Patrol", "Dvaered Small Patrol", "Dvaered Strike Force" }
-      elseif f == faction.get( "Soromid" ) then
+      elseif f == faction.get("Soromid") then
          choices = { "Soromid Arx", "Soromid Vox", "Soromid Nyx", "Soromid Odium" }
-      elseif f == faction.get( "Za'lek" ) then
+      elseif f == faction.get("Za'lek") then
          choices = { "Za'lek Hephaestus", "Za'lek Mephisto", "Za'lek Diablo", "Za'lek Demon" }
-      elseif f == faction.get( "Sirius" ) then
+      elseif f == faction.get("Sirius") then
          choices = { "Sirius Preacher", "Sirius Divinity", "Sirius Dogma" }
-      elseif f == faction.get( "Frontier" ) then
+      elseif f == faction.get("Frontier") then
          choices = { "Frontier Phalanx", "Frontier Lancelot", "Frontier Ancestor" }
-      elseif f == faction.get( "Thurion" ) then
+      elseif f == faction.get("Thurion") then
          choices = { "Thurion Apprehension", "Thurion Certitude" }
-      elseif f == faction.get( "Proteron" ) then
+      elseif f == faction.get("Proteron") then
          choices = { "Proteron Kahan", "Proteron Watson", "Proteron Archimedes" }
-      elseif f == faction.get( "Collective" ) then
+      elseif f == faction.get("Collective") then
          choices = { "Collective Lge Swarm", "Collective Sml Swarm" }
-      elseif f == faction.get( "FLF" ) then
+      elseif f == faction.get("FLF") then
          choices = { "FLF Pacifier", "FLF Vendetta", "FLF Lancelot" }
-      elseif f == faction.get( "Pirate" ) then
+      elseif f == faction.get("Pirate") then
          choices = { "Pirate Kestrel", "Pirate Phalanx", "Pirate Admonisher" }
       else
          choices = { "Vendetta Quartet", "Mercenary Pacifier", "Mercenary Ancestor", "Mercenary Vendetta" }
       end
 
-      for n = 1, rnd.rnd( 2, 4 ) do
-         for i, j in ipairs( system.cur():jumps() ) do
-            local p = pilot.addFleet( choices[ rnd.rnd( 1, #choices ) ], j:dest() )
-            for k, v in ipairs( p ) do
+      for n = 1, rnd.rnd(2, 4) do
+         for i, j in ipairs(system.cur():jumps()) do
+            local p = pilot.addFleet(choices[ rnd.rnd(1, #choices) ], j:dest())
+            for k, v in ipairs(p) do
                v:setHostile()
             end
          end
       end
 
       -- No landing, filthy waste dumper!
-      player.allowLand( false, noland_msg )
+      player.allowLand(false, noland_msg)
 
-      misn.finish( true )
+      misn.finish(true)
    end
 end
