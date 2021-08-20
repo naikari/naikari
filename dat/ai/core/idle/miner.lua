7b21d78b78fa8e74a1f55d429d71e1cf76f11b4b
diligentcircle <diligentcircle@riseup.net>
2021-08-20 11:49:51 -0400

    Started importing Naev's ai.core module with Naikari-specific changes.
    I don't know for sure if I'll finish this. Right now, it's not in
    a working state as it still references safe lanes stuff (which Naikari
    doesn't have), and I believe there are some Naikari AI features that
    don't exist in Naev's code, so I'll need to port those over. In any
    case, these files are out of the way and currently unused. If I finish
    this, I can switch all the actual AI files to use it and make Naikari's
    code base in sync with Naev's.
    
    Main issue is deciding what to do in the places where the safelanes
    system is being used to guide loitering in Naev. It's only a couple
    places so it shouldn't be too terribly hard, but I've got a bit of
    a headache right now and am a bit exhausted, so I'll hopefully finish
    up later.

---
+++ dat/ai/core/idle/miner.lua
@@ -0,0 +1,20 @@
+-- Default task to run when idle
+function idle ()
+   local field, ast = system.asteroid() -- Get a random asteroid in the system
+
+   if ai.pilot():cargoFree() == 0 or field == nil then -- Leave this system
+      local planet = ai.landplanet( mem.land_friendly )
+      -- planet must exist
+      if planet == nil then
+         ai.settimer(0, rnd.uniform(1.0, 3.0))
+         ai.pushtask("enterdelay")
+      else
+         ai.pushtask("hyperspace")
+         ai.pushtask("land", planet)
+      end
+
+   else -- Mine the asteroid
+      ai.pushtask( "mine", {field, ast} )
+   end
+
+end
