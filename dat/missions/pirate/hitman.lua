fb7652ef9beaadf154c76edd582146414eb72a8f
diligentcircle <diligentcircle@riseup.net>
2021-10-25 14:30:24 -0400

    Fixed some things in and generally touched up the hitman missions.

--- dat/missions/pirate/hitman.lua
+++ dat/missions/pirate/hitman.lua
@@ -26,16 +26,14 @@ local fmt = require "fmt"
 require "missions/pirate/common"
 
 
--- Bar information
 bar_desc = _("You see a shifty looking man sitting in a darkened corner of the bar. He is trying to discreetly motion you to join him, but is only managing to make himself look suspicious. Perhaps he's watched too many holovideos.")
 
--- Mission details
 misn_title = _("Shifty Business")
 misn_reward = _("Some easy money")
 misn_desc = _("A shifty businessman has tasked you with chasing away merchant competition in the {system} system.")
 
 osd_desc = {}
-osd_desc_1 = _("Attack, but do not kill, Trader pilots in the %s system so that they run away ({number} remaining)")
+osd_desc_1 = _("Attack, but do not kill, Trader pilots in the {system} system so that they run away ({done}/{needed})")
 osd_desc[2] = _("Land on {planet} ({system} system)")
 osd_desc["__save"] = true
 
@@ -44,40 +42,28 @@ ask_text = _([[The man motions for you to take a seat next to him. Voice barely 
 explain_text = _([[Apparently relieved that you've accepted his offer, he continues, "There're some new merchants edging in on my trade routes in {system}. I want you to make sure they know they're not welcome." Pausing for a moment, he notes, "You don't have to kill anyone, just rough them up a bit."]])
 pay_text = _([[As you inform your acquaintance that you successfully scared off the traders, he grins and transfers a sum of credits to your account. "That should teach them to stay out of my space."]])
 
--- Messages
-success_msg = _("MISSION SUCCESS! Return for payment.")
-
 log_text = _([[You chased away a shifty merchant's competition and were paid a sum of credits by the shifty merchant for your services.]])
 
 
-function create ()
+function create()
    -- Note: this mission does not make any system claims. 
-   targetsystem = system.get("Delta Pavonis") -- Find target system
-
-   -- Spaceport bar stuff
+   targetsystem = system.get("Delta Pavonis")
    misn.setNPC(_("Shifty Trader"), "neutral/unique/shifty_merchant.png", bar_desc)
 end
 
 
---[[
-Mission entry point.
---]]
-function accept ()
-   -- Mission details:
-   if not tk.yesno("", string.format(ask_text,
-          targetsystem:name())) then
+function accept()
+   if not tk.yesno("", ask_text) then
       misn.finish()
    end
    misn.accept()
    tk.msg("", fmt.f(explain_text, {system=targetsystem:name()}))
 
-   -- Some variables for keeping track of the mission
    misn_done = false
    fledTraders = 0
    tradersNeeded = 5
    misn_base, misn_base_sys = planet.cur()
 
-   -- Set mission details
    misn.setTitle(misn_title)
    misn.setReward(misn_reward)
    misn.setDesc(fmt.f(misn_desc, {system=targetsystem:name()}))
@@ -85,30 +71,28 @@ function accept ()
    misn_marker = misn.markerAdd(targetsystem, "low")
 
    osd_desc[1] = fmt.f(osd_desc_1,
-         {system=targetsystem:name(), number=tradersNeeded-fledTraders})
+         {system=targetsystem:name(), done=fledTraders, needed=tradersNeeded})
    osd_desc[2] = fmt.f(osd_desc[2], {planet=misn_base:name(), system=misn_base_sys:name()})
    misn.osdCreate(misn_title, osd_desc)
 
-   -- Set hooks
    hook.enter("sys_enter")
 end
 
--- Entering a system
-function sys_enter ()
+
+function sys_enter()
    cur_sys = system.cur()
-   -- Check to see if reaching target system
    if cur_sys == targetsystem then
       hook.pilot(nil, "attacked", "trader_attacked")
    end
 end
 
--- Attacked a trader
-function trader_attacked (hook_pilot, hook_attacker, hook_arg)
+
+function trader_attacked(hook_pilot, hook_attacker, hook_arg)
    if misn_done then
       return
    end
 
-   if hook_pilot:faction() == faction.get("Trader")
+   if hook_pilot:faction() == faction.get("Trader") and hook_attacker ~= nil
          and (hook_attacker == player.pilot()
             or hook_attacker:leader() == player.pilot()) then
       hook_pilot:hookClear()
@@ -117,8 +101,8 @@ function trader_attacked (hook_pilot, hook_attacker, hook_arg)
    end
 end
 
--- An attacked Trader Jumped
-function trader_jumped (hook_pilot, hook_arg)
+
+function trader_jumped(hook_pilot, hook_arg)
    if misn_done then
       return
    end
@@ -128,25 +112,25 @@ function trader_jumped (hook_pilot, hook_arg)
       attack_finished()
    else
       misn.osdDestroy()
-      osd_desc[1] = fmt.f(osd_desc_1,
-            {system=targetsystem:name(), number=tradersNeeded-fledTraders})
+      osd_desc[1] = fmt.f(osd_desc_1, {system=targetsystem:name(),
+               done=fledTraders, needed=tradersNeeded})
       misn.osdCreate(misn_title, osd_desc)
+      misn.osdActive(1)
    end
 end
 
--- attack finished
+
 function attack_finished()
    if misn_done then
       return
    end
    misn_done = true
-   player.msg(success_msg)
    misn.markerMove(misn_marker, misn_base_sys)
    misn.osdActive(2)
    hook.land("landed")
 end
 
--- landed
+
 function landed()
    if planet.cur() == misn_base then
       tk.msg("", pay_text)
--- dat/missions/pirate/hitman2.lua
+++ dat/missions/pirate/hitman2.lua
@@ -23,117 +23,108 @@
 
 --]]
 
+local fmt = require "fmt"
 require "missions/pirate/common"
 
 
--- Bar information
-bar_desc = _("You see the shifty merchant who hired you previously. He looks somewhat anxious, perhaps he has more business to discuss.")
+bar_desc = _("You see the shifty merchant who hired you previously. He looks somewhat anxious; perhaps he has more business to discuss.")
 
--- Mission details
-misn_title  = _("Assassin")
+misn_title  = _("Deadly Business")
 misn_reward = _("Some easy money")
-misn_desc = _("A shifty businessman has tasked you with killing merchant competition in the %s system.")
-osd_desc_1 = _("Kill Trader pilots in the %s system")
-osd_desc_2 = _("Return to %s in the %s system for payment")
+misn_desc = _("A shifty businessman has tasked you with killing merchant competition in the {system} system.")
 
--- Text
-title    = {}
-text     = {}
-title[1] = _("Spaceport Bar")
-text[1]  = _([[As you approach, the man turns to face you and his anxiousness seems to abate somewhat. As you take a seat he greets you, "Ah, so we meet again. My, shall we say... problem, has recurred." Leaning closer, he continues, "This will be somewhat bloodier than last time, but I'll pay you more for your trouble. Are you up for it?"]])
-text[2] = _([[He nods approvingly. "It seems that the traders are rather stubborn, they didn't get the message last time and their presence is increasing." He lets out a brief sigh before continuing, "This simply won't do, it's bad for business. Perhaps if a few of their ships disappear, they'll take the hint." With the arrangement in place, he gets up. "I look forward to seeing you soon. Hopefully this will be the end of my problems."]])
-title[3] = _("Mission Complete")
-text[3] = _([[You glance around, looking for your acquaintance, but he has noticed you first, motioning for you to join him. As you approach the table, he smirks. "I hope the Empire didn't give you too much trouble." After a short pause, he continues, "The payment has been transferred. Much as I enjoy working with you, hopefully this is the last time I'll require your services."]])
+osd_desc = {}
+osd_desc_1 = _("Kill Trader pilots in the {system} system ({done}/{needed})")
+osd_desc[2] = _("Land on {planet} ({system} system)")
+osd_desc["__save"] = true
 
--- Messages
-msg      = {}
-msg[1]   = _("MISSION SUCCESS! Return for payment.")
+ask_text = _([[As you approach, the man turns to face you and his anxiousness seems to abate somewhat. As you take a seat he greets you, "Ah, so we meet again. My… shall we say, problem… has recurred." Leaning closer, he continues, "This will be somewhat bloodier than last time, but I'll pay you more for your trouble. Are you up for it?"]])
+yes_text = _([[He nods approvingly. "It seems that the traders are rather stubborn, they didn't get the message last time and their presence is increasing." He lets out a brief sigh before continuing, "This simply won't do, it's bad for business. Perhaps if a few of their ships disappear, they'll take the hint." With the arrangement in place, he gets up. "I look forward to seeing you soon. Hopefully this will be the end of my problems."]])
+pay_text = _([[You glance around, looking for your acquaintance, but he has noticed you first, motioning for you to join him. As you approach the table, he smirks. "I hope the Empire didn't give you too much trouble." After a short pause, he continues, "The payment has been transferred. Much as I enjoy working with you, hopefully this is the last time I'll require your services."]])
 
 log_text = _([[You assassinated some of the shifty merchant's competition and were paid a sum of credits for your services. He said that he should hopefully not require further services from you.]])
 
 
-function create ()
+function create()
    -- Note: this mission does not make any system claims. 
-   targetsystem = system.get("Delta Pavonis") -- Find target system
-
-   -- Spaceport bar stuff
-   misn.setNPC( _("Shifty Trader"),  "neutral/unique/shifty_merchant.png", bar_desc)
+   targetsystem = system.get("Delta Pavonis")
+   misn.setNPC(_("Shifty Trader"), "neutral/unique/shifty_merchant.png", bar_desc)
 end
 
 
---[[
-Mission entry point.
---]]
-function accept ()
-   -- Mission details:
-   if not tk.yesno( title[1], string.format( text[1],
-          targetsystem:name() ) ) then
+function accept()
+   if not tk.yesno("", ask_text) then
       misn.finish()
    end
    misn.accept()
+   tk.msg("", yes_text)
 
-   -- Some variables for keeping track of the mission
    misn_done      = false
    attackedTraders = {}
    attackedTraders["__save"] = true
    deadTraders = 0
+   tradersNeeded = 3
    misn_base, misn_base_sys = planet.cur()
 
-   -- Set mission details
-   misn.setTitle( string.format( misn_title, targetsystem:name()) )
-   misn.setReward( string.format( misn_reward, credits) )
-   misn.setDesc( string.format( misn_desc, targetsystem:name() ) )
-   misn_marker = misn.markerAdd( targetsystem, "low" )
-   local osd_desc = {}
-   osd_desc[1] = osd_desc_1:format( targetsystem:name() )
-   osd_desc[2] = osd_desc_2:format( misn_base:name(), misn_base_sys:name() )
-   misn.osdCreate( misn_title, osd_desc )
-   -- Some flavour text
-   tk.msg( title[1], string.format( text[2], targetsystem:name()) )
+   misn.setTitle(misn_title)
+   misn.setReward(misn_reward)
+   misn.setDesc(fmt.f(misn_desc, {system=targetsystem:name()}))
 
-   -- Set hooks
+   misn_marker = misn.markerAdd(targetsystem, "low")
+
+   osd_desc[1] = fmt.f(osd_desc_1,
+         {system=targetsystem:name(), done=deadTraders, needed=tradersNeeded})
+   osd_desc[2] = fmt.f(osd_desc[2], {planet=misn_base:name(), system=misn_base_sys:name()})
+   misn.osdCreate(misn_title, osd_desc)
+
    hook.enter("sys_enter")
 end
 
--- Entering a system
-function sys_enter ()
+
+function sys_enter()
    cur_sys = system.cur()
-   -- Check to see if reaching target system
    if cur_sys == targetsystem then
       hook.pilot(nil, "death", "trader_death")
    end
 end
 
--- killed a trader
-function trader_death (hook_pilot, hook_attacker, hook_arg)
+
+function trader_death(hook_pilot, hook_attacker, hook_arg)
    if misn_done then
       return
    end
 
-   if hook_pilot:faction() == faction.get("Trader")
-         and ( hook_attacker == player.pilot()
-            or hook_attacker:leader() == player.pilot() ) then
-      attack_finished()
+   if hook_pilot:faction() == faction.get("Trader") and hook_attacker ~= nil
+         and (hook_attacker == player.pilot()
+            or hook_attacker:leader() == player.pilot()) then
+      deadTraders = deadTraders + 1
+      if deadTraders >= tradersNeeded then
+         attack_finished()
+      else
+         misn.osdDestroy()
+         osd_desc[1] = fmt.f(osd_desc_1, {system=targetsystem:name(),
+                  done=deadTraders, needed=tradersNeeded})
+         misn.osdCreate(misn_title, osd_desc)
+         misn.osdActive(1)
+      end
    end
 end
 
--- attack finished
+
 function attack_finished()
    if misn_done then
       return
    end
    misn_done = true
-   player.msg( msg[1] )
-   misn.markerRm( misn_marker )
-   misn_marker = misn.markerAdd( misn_base_sys, "low" )
+   misn.markerMove(misn_marker, misn_base_sys)
    misn.osdActive(2)
    hook.land("landed")
 end
 
--- landed
+
 function landed()
    if planet.cur() == misn_base then
-      tk.msg(title[3], text[3])
+      tk.msg("", pay_text)
       player.pay(500000) -- 500K
       pir_modReputation(3)
       faction.modPlayer("Pirate", 2)
