ccc5a631eb6c16fa38fa9cc247490de22c0ea1bb
Edgar Simo-Serra <bobbens@gmail.com>
2021-11-24 11:46:07 +0900

    Depth for the rendering SDF stuff. Fixes #1629.

--- src/font.c
+++ src/font.c
@@ -1236,6 +1236,10 @@ static void gl_fontRenderStartH( const glFontStash* stsh, const gl_Matrix4 *H, c
    gl_vboActivateAttribOffset( stsh->vbo_vert, shaders.font.vertex, 0, 2, GL_SHORT, 0 );
    glEnableVertexAttribArray( shaders.font.tex_coord );
    gl_vboActivateAttribOffset( stsh->vbo_tex, shaders.font.tex_coord, 0, 2, GL_FLOAT, 0 );
+
+   /* Set up depth stuff. */
+   glClear( GL_DEPTH_BUFFER_BIT );
+   glEnable( GL_DEPTH_TEST );
 }
 
 
@@ -1455,6 +1459,8 @@ static void gl_fontRenderEnd (void)
    glDisableVertexAttribArray( shaders.font.vertex );
    glDisableVertexAttribArray( shaders.font.tex_coord );
    glUseProgram(0);
+
+   glDisable( GL_DEPTH_TEST );
 
    /* Check for errors. */
    gl_checkErr();
