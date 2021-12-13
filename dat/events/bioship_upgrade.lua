169154d4d7837fe7d240e2975475613737acd291
diligentcircle <diligentcircle@riseup.net>
2021-12-12 23:11:29 -0500

    Fixed bioship upgrade event and made it tell the player about XP gains.

--- dat/events/bioship_upgrade.lua
+++ dat/events/bioship_upgrade.lua
@@ -39,6 +39,8 @@
 
 --]]
 
+local fmt = require "fmt"
+
 
 -- A table of all part types. Each entry is another table with
 -- two entries. The first entry in each inner table is the base name of
@@ -48,52 +50,54 @@
 -- final stage (Stage X).
 bioship_parts = {
    -- Brains (Core System analog)
-   { "Ultralight Brain Stage %s", 2 },
-   { "Light Brain Stage %s", 3 },
-   { "Medium Brain Stage %s", 4 },
-   { "Medium-Heavy Brain Stage %s", 5 },
-   { "Heavy Brain Stage %s", 6 },
-   { "Superheavy Brain Stage %s", 7 },
+   {"Ultralight Brain Stage %s", 2},
+   {"Light Brain Stage %s", 3},
+   {"Medium Brain Stage %s", 4},
+   {"Medium-Heavy Brain Stage %s", 5},
+   {"Heavy Brain Stage %s", 6},
+   {"Superheavy Brain Stage %s", 7},
 
    -- Shells (Hull analog)
-   { "Ultralight Shell Stage %s", 2 },
-   { "Light Shell Stage %s", 3 },
-   { "Medium Shell Stage %s", 4 },
-   { "Medium-Heavy Shell Stage %s", 5 },
-   { "Heavy Shell Stage %s", 6 },
-   { "Superheavy Shell Stage %s", 7 },
+   {"Ultralight Shell Stage %s", 2},
+   {"Light Shell Stage %s", 3},
+   {"Medium Shell Stage %s", 4},
+   {"Medium-Heavy Shell Stage %s", 5},
+   {"Heavy Shell Stage %s", 6},
+   {"Superheavy Shell Stage %s", 7},
 
    -- Fins (Engine analog)
-   { "Ultralight Fast Fin Stage %s", 2 },
-   { "Light Fast Fin Stage %s", 3 },
-   { "Medium Fast Fin Stage %s", 4 },
-   { "Medium-Heavy Fast Fin Stage %s", 5 },
-   { "Heavy Fast Fin Stage %s", 6 },
-   { "Superheavy Fast Fin Stage %s", 7 },
-   { "Ultralight Strong Fin Stage %s", 2 },
-   { "Light Strong Fin Stage %s", 3 },
-   { "Medium Strong Fin Stage %s", 4 },
-   { "Medium-Heavy Strong Fin Stage %s", 5 },
-   { "Heavy Strong Fin Stage %s", 6 },
-   { "Superheavy Strong Fin Stage %s", 7 },
+   {"Ultralight Fast Fin Stage %s", 2},
+   {"Light Fast Fin Stage %s", 3},
+   {"Medium Fast Fin Stage %s", 4},
+   {"Medium-Heavy Fast Fin Stage %s", 5},
+   {"Heavy Fast Fin Stage %s", 6},
+   {"Superheavy Fast Fin Stage %s", 7},
+   {"Ultralight Strong Fin Stage %s", 2},
+   {"Light Strong Fin Stage %s", 3},
+   {"Medium Strong Fin Stage %s", 4},
+   {"Medium-Heavy Strong Fin Stage %s", 5},
+   {"Heavy Strong Fin Stage %s", 6},
+   {"Superheavy Strong Fin Stage %s", 7},
 
    -- Weapons
-   { "BioPlasma Stinger Stage %s", 3 },
-   { "BioPlasma Claw Stage %s", 4 },
-   { "BioPlasma Fang Stage %s", 5 },
-   { "BioPlasma Talon Stage %s", 6 },
-   { "BioPlasma Tentacle Stage %s", 7 },
+   {"BioPlasma Stinger Stage %s", 3},
+   {"BioPlasma Claw Stage %s", 4},
+   {"BioPlasma Fang Stage %s", 5},
+   {"BioPlasma Talon Stage %s", 6},
+   {"BioPlasma Tentacle Stage %s", 7},
 }
 
 
-function create ()
-   hook.pay( "pay" )
+function create()
+   shiplog.create("bioship", _("Bioship Log"), _("Ship"), false, 50)
+
+   hook.pay("pay")
 end
 
 
-function is_bioship_part( s )
-   for i, p in ipairs( bioship_parts ) do
-      if string.match( s, p[1]:format( ".*" ) ) then
+function is_bioship_part(s)
+   for i, p in ipairs(bioship_parts) do
+      if string.match(s, p[1]:format(".*")) then
          return true
       end
    end
@@ -103,10 +107,10 @@ end
 
 -- Returns the index of bioship_parts the given outfit is a part of if a
 -- valid undeveloped part, or nil otherwise.
-function undeveloped_bioship_part_index( s )
-   for i, p in ipairs( bioship_parts ) do
+function undeveloped_bioship_part_index(s)
+   for i, p in ipairs(bioship_parts) do
       local pat = p[1]:gsub ("-", "[-]")
-      if string.match( s, pat:format( "%d" ) ) then
+      if string.match(s, pat:format("%d")) then
          return i
       end
    end
@@ -114,9 +118,9 @@ function undeveloped_bioship_part_index( s )
 end
 
 
-function has_bioship ()
-   for i, o in ipairs( player.pilot():outfits() ) do
-      if is_bioship_part( o:nameRaw() ) then
+function has_bioship()
+   for i, o in ipairs(player.pilot():outfits()) do
+      if is_bioship_part(o:nameRaw()) then
          return true
       end
    end
@@ -128,37 +132,51 @@ end
 -- the player; each inner table contains, respectively:
 --    * The outfit name
 --    * The corresponding index in bioship_parts
-function get_bioship_parts ()
+function get_bioship_parts()
    local parts = {}
-   for i, o in ipairs( player.pilot():outfits() ) do
-      local index = undeveloped_bioship_part_index( o:nameRaw() )
+   for i, o in ipairs(player.pilot():outfits()) do
+      local index = undeveloped_bioship_part_index(o:nameRaw())
       if index ~= nil then
-         parts[ #parts + 1 ] = { o:nameRaw(), index }
+         parts[ #parts + 1 ] = {o:nameRaw(), index}
       end
    end
    return parts
 end
 
 
-function pay( amount, reason )
+function log_entry(text)
+   player.msg(fmt.f(_("#oBioship Log: {text}#0"), {text=text}))
+   shiplog.append("bioship", text)
+end
+
+
+function pay(amount, reason)
    local pp = player.pilot()
    local exp_gain = math.floor(amount / 10000)
    if amount > 0 and reason ~= "adjust" and reason ~= "loot"
          and has_bioship() then
-      local exp = var.peek( "_bioship_exp" ) or 0
+      local exp = var.peek("_bioship_exp") or 0
       exp = exp + exp_gain
+      local s = string.format(
+            n_("Gained %d XP", "Gained %d XP", exp_gain), exp_gain)
+      if exp < 100 then
+         s = s .. string.format(n_(" (%d more XP until next upgrade)",
+               " (%d more XP until next upgrade)", 100 - exp), 100 - exp)
+      end
+      log_entry(s)
+
       while exp >= 100 do
          exp = exp - 100
          local parts = get_bioship_parts()
          if #parts > 0 then
-            local part_t = parts[ rnd.rnd( 1, #parts ) ]
+            local part_t = parts[ rnd.rnd(1, #parts) ]
             local part = part_t[1]
             local index = part_t[2]
             local current_level = 0
             local max_level = bioship_parts[index][2]
 
             for i=1,max_level do
-               if part == bioship_parts[index][1]:format( string.format( "%d", i ) ) then
+               if part == bioship_parts[index][1]:format(string.format("%d", i)) then
                   current_level = i
                   break
                end
@@ -166,26 +184,35 @@ function pay( amount, reason )
 
             local new_level = current_level + 1
 
-            pp:outfitRm( part )
+            pp:outfitRm(part)
             local new_part
             if new_level > max_level then
-               new_part = bioship_parts[index][1]:format( "X" ) 
+               new_part = bioship_parts[index][1]:format("X") 
             else
-               local sn = string.format( "%d", new_level )
-               new_part = bioship_parts[index][1]:format( sn )
+               local sn = string.format("%d", new_level)
+               new_part = bioship_parts[index][1]:format(sn)
             end
-            local q = pp:outfitAdd( new_part, 1, true ) -- Only check slot stuff, ignore CPU and the rest.
+            local q = pp:outfitAdd(new_part, 1, true) -- Only check slot stuff, ignore CPU and the rest.
             if q<=0 then
                warn(string.format(_("Unable to upgrade Soromid outfit to %s!"), new_part))
-               pp:outfitAdd( part, 1, true ) -- Try to add previous one
+               pp:outfitAdd(part, 1, true) -- Try to add previous one
+            else
+               log_entry(fmt.f(_("Upgraded {old_part} to {new_part}"),
+                     {old_part=part, new_part=new_part}))
             end
 
             -- Reset stats since we leveled up (prevents gameplay problems)
-            pp:setHealth( 100, 100 )
-            pp:setEnergy( 100 )
-            pp:setTemp( 0 )
-            pp:setFuel( true )
+            pp:setHealth(100, 100)
+            pp:setEnergy(100)
+            pp:setTemp(0)
+            pp:setFuel(true)
          end
-      end 
+         if exp < 100 then
+            log_entry(string.format(n_("%d more XP until next upgrade",
+                  "%d more XP until next upgrade", 100 - exp), 100 - exp))
+         end
+      end
+
+      var.push("_bioship_exp", exp)
    end
 end
