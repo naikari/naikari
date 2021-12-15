3ee1c03cda4961be785af738179c2ff7405d870c
diligentcircle <diligentcircle@riseup.net>
2021-12-15 17:33:10 -0500

    Increased alt text width a bit.

--- src/toolkit.c
+++ src/toolkit.c
@@ -1376,7 +1376,7 @@ void toolkit_drawAltText( int bx, int by, const char *alt )
    glClear( GL_DEPTH_BUFFER_BIT );
 
    /* Get dimensions. */
-   w = 250;
+   w = 350;
    h = gl_printHeightRaw( &gl_defFont, w, alt );
 
    /* Choose position. */
