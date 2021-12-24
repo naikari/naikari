53c5f73207e194ec0718c2ccf99dcd96db6c512e
diligentcircle <diligentcircle@riseup.net>
2021-12-24 16:53:04 -0500

    Removed the spawning of tons of ships when illegally dumping waste.
    Too reliant on the deprecated fleet system and doesn't make much
    sense anyway, so now it just makes everyone in the system pissed at
    you.

--- dat/missions/neutral/wastedump.lua
+++ dat/missions/neutral/wastedump.lua
@@ -190,60 +190,8 @@ function abort ()
 
    misn.cargoJet(cid)
 
-   -- Make everyone angry
    for i, p in ipairs(pilot.get()) do
       p:setHostile()
-   end
-
-   -- Add some police!
-   local presences = system.cur():presences()
-   local f = nil
-   if presences then
-      local strongest_amount = 0
-      for k, v in pairs(presences) do
-         if v > strongest_amount then
-            f = faction.get(k)
-            strongest_amount = v
-         end
-      end
-   end
-
-   local choices
-   if f == faction.get("Empire") then
-      choices = {"Empire Sml Defense", "Empire Lge Attack", "Empire Med Attack"}
-   elseif f == faction.get("Goddard") then
-      choices = {"Goddard Goddard", "Goddard Lancelot"}
-   elseif f == faction.get("Dvaered") then
-      choices = {"Dvaered Big Patrol", "Dvaered Small Patrol", "Dvaered Strike Force"}
-   elseif f == faction.get("Soromid") then
-      choices = {"Soromid Arx", "Soromid Vox", "Soromid Nyx", "Soromid Odium"}
-   elseif f == faction.get("Za'lek") then
-      choices = {"Za'lek Hephaestus", "Za'lek Mephisto", "Za'lek Diablo", "Za'lek Demon"}
-   elseif f == faction.get("Sirius") then
-      choices = {"Sirius Preacher", "Sirius Divinity", "Sirius Dogma"}
-   elseif f == faction.get("Frontier") then
-      choices = {"Frontier Phalanx", "Frontier Lancelot", "Frontier Ancestor"}
-   elseif f == faction.get("Thurion") then
-      choices = {"Thurion Apprehension", "Thurion Certitude"}
-   elseif f == faction.get("Proteron") then
-      choices = {"Proteron Kahan", "Proteron Watson", "Proteron Archimedes"}
-   elseif f == faction.get("Collective") then
-      choices = {"Collective Lge Swarm", "Collective Sml Swarm"}
-   elseif f == faction.get("FLF") then
-      choices = {"FLF Pacifier", "FLF Vendetta", "FLF Lancelot"}
-   elseif f == faction.get("Pirate") then
-      choices = {"Pirate Kestrel", "Pirate Phalanx", "Pirate Admonisher"}
-   else
-      choices = {"Vendetta Quartet", "Mercenary Pacifier", "Mercenary Ancestor", "Mercenary Vendetta"}
-   end
-
-   for n = 1, rnd.rnd(2, 4) do
-      for i, j in ipairs(system.cur():jumps()) do
-         local p = pilot.addFleet(choices[rnd.rnd(1, #choices)], j:dest())
-         for k, v in ipairs(p) do
-            v:setHostile()
-         end
-      end
    end
 
    -- No landing, filthy waste dumper!
