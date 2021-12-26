3742f1d72bf7e20a73e92926bd7e9d503ce5e6dd
diligentcircle <diligentcircle@riseup.net>
2021-12-25 21:08:10 -0500

    Shortened sh00's OSD messages and converted to fmt module.

--- dat/missions/shark/sh00_ancestorkill.lua
+++ dat/missions/shark/sh00_ancestorkill.lua
@@ -36,39 +36,34 @@
 
 --]]
 
---Needed scripts
+local fmt = require "fmt"
 require "pilot/pirate"
-require "numstring"
 require "missions/shark/common"
 
 
-title = {}
-text = {}
-osd_msg = {}
-npc_desc = {}
-bar_desc = {}
-
-text[1] = _([[You approach the man and he introduces himself. "Hello, my name is Arnold Smith; I work for Nexus Shipyards. I'm looking for a talented pilot to make a demonstration to one of our potential customers.
+ask_text = _([[You approach the man and he introduces himself. "Hello, my name is Arnold Smith; I work for Nexus Shipyards. I'm looking for a talented pilot to make a demonstration to one of our potential customers.
 
 "Pretty simple, really: we want someone to show how great Nexus ship designs are by destroying a Pirate Ancestor with just one of our smallest ship model, the Shark. Of course, the pilot of the Ancestor has a bounty on his head, so it won't be illegal. The sum of the bounty will be paid to you and Nexus will add a little extra. Would you be interested?"]])
 
 refusetext = _([["That's your choice," the man says. "Don't hesitate to tell me if you change your mind."]])
 
-text[2] = _([["Great! I knew I could trust you. I'll meet you on %s in the %s system. I'll be with my boss and our customer, Baron Sauterfeldt."]])
+yes_text = _([["Great! I knew I could trust you. I'll meet you on {planet} in the {system} system. I'll be with my boss and our customer, Baron Sauterfeldt."]])
 
-text[3] = _([["Nice to see you again," he says with a smile. "I hope you are ready to kick that pirate's ass! Please follow me. I will introduce you to my boss, the sales manager of Nexus Shipyards. Oh, and the Baron, too."
+brief_text = _([["Nice to see you again," he says with a smile. "I hope you are ready to kick that pirate's ass! Please follow me. I will introduce you to my boss, the sales manager of Nexus Shipyards. Oh, and the Baron, too."
 
 Arnold Smith guides you to some kind of control room where you see some important-looking people. After introducing you to some of them, he goes over the mission, rather over-emphasizing the threat involved; it's just a Pirate Ancestor, after all. Nonetheless, the Baron is intrigued.
 
-Arnold Smith gets a call. After answering, he turns to you. "Perfect timing! The pirate has just arrived at %s. Now go show them what your ship can do!" Time to head back to the ship, then.]])
+Arnold Smith gets a call. After answering, he turns to you. "Perfect timing! The pirate has just arrived at {system}. Now go show them what your ship can do!" Time to head back to the ship, then.]])
 
-text[4] = _([[As you step on the ground, Arnold Smith greets you. "That was a great demonstration! Thank you. I haven't been able to speak to the Baron about the results yet, but I am confident he will be impressed." He hands you your pay. "I may have another mission for you later. Be sure to check back!"]])
+pay_text = _([[As you step on the ground, Arnold Smith greets you. "That was a great demonstration! Thank you. I haven't been able to speak to the Baron about the results yet, but I am confident he will be impressed." He hands you your pay. "I may have another mission for you later. Be sure to check back!"]])
 
 -- Mission details
 misn_title = _("A Shark Bites")
 misn_desc = _("Nexus Shipyards needs you to demonstrate to Baron Sauterfeldt the capabilities of Nexus designs.")
 
 -- NPC
+npc_desc = {}
+bar_desc = {}
 npc_desc[1] = _("An honest-looking man")
 bar_desc[1] = _("This man looks like a honest citizen. He glances in your direction.")
 
@@ -77,9 +72,10 @@ bar_desc[2] = _([[The Nexus employee who recruited you for a very special demo o
 
 -- OSD
 osd_title = _("A Shark Bites")
-osd_msg[1] = _("Buy a Shark (but not a Pirate Shark), then land on %s (%s system)")
-osd_msg[2] = _("Go to the %s system and kill the pirate with your Shark")
-osd_msg[3] = _("Land on %s (%s system) and collect your fee")
+osd_msg = {}
+osd_msg[1] = _("Land on {planet} ({system} system) with a Shark (but not a Pirate Shark)")
+osd_msg[2] = _("Go to the {system} system and kill the pirate with your Shark")
+osd_msg[3] = _("Land on {planet} ({system} system)")
 
 leave_msg = _("MISSION FAILED: You left the pirate.")
 piratejump_msg = _("MISSION FAILED: The pirate ran away.")
@@ -110,17 +106,19 @@ function accept()
    stage = 0
    reward = 500000
 
-   if tk.yesno("", text[1]) then
+   if tk.yesno("", ask_text) then
       misn.accept()
-      piratename = pirate_name()    --for now, we only need his name
-      tk.msg("", text[2]:format(missys:name(),mispla:name()))
+      piratename = pirate_name()
+      tk.msg("", fmt.f(yes_text, {planet=mispla:name(), system=missys:name()}))
 
-      osd_msg[1] = osd_msg[1]:format(mispla:name(), missys:name())
-      osd_msg[2] = osd_msg[2]:format(battlesys:name())
-      osd_msg[3] = osd_msg[3]:format(mispla:name(), missys:name())
+      osd_msg[1] = fmt.f(osd_msg[1],
+            {planet=mispla:name(), system=missys:name()})
+      osd_msg[2] = fmt.f(osd_msg[2], {system=battlesys:name()})
+      osd_msg[3] = fmt.f(osd_msg[3],
+            {planet=mispla:name(), system=missys:name()})
 
       misn.setTitle(misn_title)
-      misn.setReward(creditstring(reward))
+      misn.setReward(fmt.credits(reward))
       misn.setDesc(misn_desc)
       osd = misn.osdCreate(osd_title, osd_msg)
       misn.osdActive(1)
@@ -147,7 +145,7 @@ function land()
 
    -- Did the player land again on Ulios after having killed the pirate
    if planet.cur() == mispla and stage == 4 then
-      tk.msg("", text[4])
+      tk.msg("", pay_text)
       player.pay(reward)
       misn.osdDestroy(osd)
       hook.rm(enterhook)
@@ -202,7 +200,7 @@ function beginbattle()
 
    misn.markerRm(markeri)
 
-   tk.msg("", text[3]:format(battlesys:name()))
+   tk.msg("", fmt.f(brief_text, {system=battlesys:name()}))
    misn.osdActive(2)
    stage = 1
 
