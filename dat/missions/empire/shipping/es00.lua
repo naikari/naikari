30accc8030101480777a3827f8f81271d27b4283
diligentcircle <diligentcircle@riseup.net>
2022-01-19 04:03:19 -0500

    Cleaned up es00 and improved it by making it not lose FLF rep.
    The cleanup is just the standard style updates (including using the
    fmt module and simplifying the OSD). In addition (and this was the
    initial reason for starting this commit), the FLF ships spawned by
    this mission have been changed from actual FLF ships to ships of an
    FLF-derived dynamic faction so that the player firing on them doesn't
    lead to reputation loss. This is primarily in case the player made
    the bad decision to fly over and do the prisoner exchange with a
    Destroyer or something, since this would leave the player blindsided
    and pretty much force them to kill FLF ships (unlike in the VIP
    mission where you know there's fighting breaking out from the start).
    That's kind of unfair seeing how dangerous Frontier space becomes if
    you make yourself an enemy of the FLF.

--- dat/missions/empire/shipping/es00.lua
+++ dat/missions/empire/shipping/es00.lua
@@ -26,31 +26,33 @@
 
 ]]--
 
-require "numstring"
+local fmt = require "fmt"
 local fleet = require "fleet"
 require "missions/empire/common"
 
 bar_desc = _("You see an Empire Commander. He seems to have noticed you.")
 misn_title = _("Prisoner Exchange")
-misn_desc = {}
-misn_desc[1] = _("Land on %s (%s system) to exchange prisoners with the FLF")
-misn_desc[2] = _("Land on %s (%s system) to report what happened")
+misn_desc = _("You have been tasked with performing a prisoner exchange with the FLF on behalf of the Empire. You are to exchange the FLF prisoners with Empire prisoners on {planet}, then return the Empire prisoners to {planet2}.")
 
-text = {}
-text[1] = _([[You approach the Empire Commander.
+ask_text = _([[You approach the Empire Commander.
 
-"Hello, you must be %s. I've heard about you. I'm Commander Soldner. We've got some harder missions for someone like you in the Empire Shipping division. There would be some real danger involved in these missions, unlike the ones you've recently completed for the division. Would you be up for the challenge?"]])
-text[2] = _([["We've got a prisoner exchange set up with the FLF to take place on %s in the %s system. They want a more neutral pilot to do the exchange. You would have to go to %s with some FLF prisoners aboard your ship and exchange them for some of our own. You won't have visible escorts but we will have your movements watched by ships in nearby sectors.
+"Hello, you must be {player}. I've heard about you. I'm Commander Soldner. We've got some harder missions for someone like you in the Empire Shipping division. There would be some real danger involved in these missions, unlike the ones you've recently completed for the division. Would you be up for the challenge?"]])
 
-"Once you get the soldiers they captured back, bring them over to %s in %s for debriefing. You'll be compensated for your troubles. Good luck."]])
-text[3] = _([[The Prisoners are loaded onto your ship along with a few marines to ensure nothing untoward happens.]])
-text[4] = _([[As you land, you notice the starport has been emptied. You also notice explosives rigged on some of the columns. This doesn't look good. The marines tell you to sit still while they go out to try to complete the prisoner exchange.
+accept_text = _([["We've got a prisoner exchange set up with the FLF to take place on {planet} in the {system} system. They want a more neutral pilot to do the exchange. You would have to go to {planet} with some FLF prisoners aboard your ship and exchange them for some of our own. You won't have visible escorts but we will have your movements watched by ships in nearby sectors.
+
+"Once you get the soldiers they captured back, bring them over to {planet2} in {system2} for debriefing. You'll be compensated for your troubles. Good luck."
+
+The Prisoners are loaded onto your ship along with a few marines to ensure nothing untoward happens.]])
+
+land_text = _([[As you land, you notice the starport has been emptied. You also notice explosives rigged on some of the columns. This doesn't look good. The marines tell you to sit still while they go out to try to complete the prisoner exchange.
 
 From the cockpit you see how the marines lead the prisoners in front of them with guns to their backs. You see figures step out of the shadows with weapons too; most likely the FLF.]])
-text[5] = _([[All of a sudden a siren blares and you hear shooting break out. You quickly start your engines and prepare for take off. Shots ring out all over the landing bay and you can see a couple of corpses as you leave the starport. You remember the explosives just as loud explosions go off behind you. This doesn't look good at all.
+
+land2_text = _([[All of a sudden a siren blares and you hear shooting break out. You quickly start your engines and prepare for take off. Shots ring out all over the landing bay and you can see a couple of corpses as you leave the starport. You remember the explosives just as loud explosions go off behind you. This doesn't look good at all.
 
 You start your climb out of the atmosphere and notice how you're picking up many FLF and Dvaered ships. Looks like you're going to have quite a run to get the hell out of here. It didn't go as you expected.]])
-text[6] = _([[After you leave your ship in the starport, you meet up with Commander Soldner. From the look on his face, it seems like he already knows what happened.
+
+pay_text = _([[After you leave your ship in the starport, you meet up with Commander Soldner. From the look on his face, it seems like he already knows what happened.
 
 "It was all the Dvaereds' fault. They just came in out of nowhere and started shooting. What a horrible mess. We're already working on sorting out the blame.
 
@@ -61,8 +63,8 @@ log_text = _([[You took part in a prisoner exchange with the FLF on behalf of th
 
 function create ()
    -- Target destination
-   dest,destsys = planet.getLandable(faction.get("Frontier"))
-   ret,retsys   = planet.getLandable("Halir")
+   dest, destsys = planet.getLandable(faction.get("Frontier"))
+   ret, retsys = planet.getLandable("Halir")
    if dest == nil or ret == nil or not misn.claim(destsys) then
       misn.finish(false)
    end
@@ -74,30 +76,34 @@ end
 
 function accept ()
    -- Intro text
-   if not tk.yesno("", string.format(text[1], player.name())) then
+   if not tk.yesno("", fmt.f(ask_text, {player=player.name()})) then
       misn.finish()
    end
 
    -- Accept mission
    misn.accept()
 
-   -- target destination
-   misn_marker = misn.markerAdd(destsys, "low")
-
-   -- Mission details
-   misn_stage = 0
    reward = 500000
    misn.setTitle(misn_title)
-   misn.setReward(creditstring(reward))
-   misn.setDesc(string.format(misn_desc[1], dest:name(), destsys:name()))
+   misn.setReward(fmt.credits(reward))
+   misn.setDesc(fmt.f(misn_desc, {planet=dest:name(), planet2=ret:name()}))
 
-   -- Flavour text and mini-briefing
-   tk.msg("", string.format(text[2], dest:name(), destsys:name(),
-         dest:name(), ret:name(), retsys:name()))
-   misn.osdCreate(misn_title, {misn_desc[1]:format(dest:name(),destsys:name())})
-   -- Set up the goal
+   tk.msg("", fmt.f(accept_text,
+         {planet=dest:name(), system=destsys:name(), planet2=ret:name(),
+            system2=retsys:name()}))
+
+   local osd_desc = {
+      fmt.f(_("Land on {planet} ({system} system)"),
+            {planet=dest:name(), system=destsys:name()}),
+      fmt.f(_("Land on {planet} ({system} system)"),
+            {planet=ret:name(), system=retsys:name()}),
+   }
+   misn.osdCreate(misn_title, osd_desc)
+
+   misn_marker = misn.markerAdd(destsys, "low")
+
+   misn_stage = 0
    prisoners = misn.cargoAdd("Prisoners", 0)
-   tk.msg("", text[3])
 
    -- Set hooks
    hook.land("land")
@@ -114,11 +120,10 @@ function land ()
          misn_stage = 1
 
          -- Some text
-         tk.msg("", text[4])
-         tk.msg("", text[5])
+         tk.msg("", land_text)
+         tk.msg("", land2_text)
          misn.markerMove(misn_marker, retsys)
-         misn.setDesc(string.format(misn_desc[2], ret:name(), retsys:name()))
-         misn.osdCreate(misn_title, {misn_desc[2]:format(ret:name(),retsys:name())})
+         misn.osdActive(2)
 
          -- Prevent players from saving on the destination planet
          player.allowSave(false)
@@ -136,7 +141,7 @@ function land ()
       faction.modPlayer("Empire", 2)
 
       -- Flavour text
-      tk.msg("", text[6])
+      tk.msg("", pay_text)
 
       emp_addShippingLog(log_text)
 
@@ -155,10 +160,14 @@ function enter ()
       ang = rnd.rnd(0, 360)
       enter_vect = player.pos() + vec2.newP(rnd.rnd(1500, 2000), ang)
 
+      -- Safe FLF faction so that the player doesn't lose reputation
+      -- with the FLF from doing this mission (in case the player made
+      -- the bad decision to fly a Destroyer or something here).
+      local f = faction.dynAdd("FLF", "FLF_safe", N_("FLF"))
+
       -- Create some pilots to go after the player
-      local flt = fleet.add(1, {"Lancelot", "Vendetta", "Vendetta"}, "FLF",
-            enter_vect, {_("FLF Lancelot"), _("FLF Vendetta"),
-               _("FLF Vendetta")})
+      local flt = fleet.add({1, 2}, {"Lancelot", "Vendetta"}, f, enter_vect,
+            {_("FLF Lancelot"), _("FLF Vendetta")})
       -- Set hostile
       for i, p in ipairs(flt) do
          p:setHostile()
@@ -170,15 +179,14 @@ function enter ()
 
       -- We'll put the FLF first
       enter_vect = battle_pos + vec2.newP(rnd.rnd(700, 1000), rnd.rnd(0, 360))
-      
-      fleet.add(1, {"Pacifier", "Lancelot", "Lancelot", "Vendetta", "Vendetta"},
-            "FLF", enter_vect, {_("FLF Pacifier"), _("FLF Lancelot"),
-               _("FLF Lancelot"), _("FLF Vendetta"), _("FLF Vendetta")})
+
+      fleet.add({1, 2, 2}, {"Pacifier", "Lancelot", "Vendetta"}, f, enter_vect,
+            {_("FLF Pacifier"), _("FLF Lancelot"), _("FLF Vendetta")})
 
       -- Now the Dvaered
       enter_vect = battle_pos + vec2.newP(rnd.rnd(200, 300), rnd.rnd(0, 360))
-      fleet.add(1, {"Dvaered Vigilance", "Dvaered Phalanx", "Dvaered Ancestor",
-               "Dvaered Ancestor", "Dvaered Vendetta", "Dvaered Vendetta"},
+      fleet.add({1, 1, 2, 2}, {"Dvaered Vigilance", "Dvaered Phalanx",
+               "Dvaered Ancestor", "Dvaered Vendetta"},
             "Dvaered", enter_vect)
 
       -- Player should not be able to reland
