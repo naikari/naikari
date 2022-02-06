naev/naev

Use mktemp when no build path is specified.

utils/buildAppImage.sh
@@ -2,22 +2,19 @@
 
 # AppImage BUILD SCRIPT FOR NAEV
 #
-# Written by Jack Greiner (ProjectSynchro on Github: https://github.com/ProjectSynchro/)
-#
 # For more information, see http://appimage.org/
-# Pass in [-d] [-c] (set this for debug builds) [-n] (set this for nightly builds) -s <SOURCEROOT> (Sets location of source) -b <BUILDROOT> (Sets location of build directory)
+# Pass in [-d] [-c] (set this for debug builds) [-n] (set this for nightly builds) -s <SOURCEPATH> (Sets location of source) -b <WORKPATH> (Sets location of build directory)
 
-# Output destination is ${BUILDPATH}/dist
+# Output destination is ${WORKPATH}/dist
 
 set -e
 
 # Defaults
-SOURCEROOT="$(pwd)"
-BUILDPATH="$(pwd)/build/appimageBuild"
+SOURCEPATH="$(pwd)"
 NIGHTLY="false"
 BUILDTYPE="release"
 
-while getopts dcnms:b:o: OPTION "$@"; do
+while getopts dcns:b: OPTION "$@"; do
     case $OPTION in
     d)
         set -x
@@ -30,37 +27,62 @@ while getopts dcnms:b:o: OPTION "$@"; do
         BUILDTYPE="debug"
         ;;
     s)
-        SOURCEROOT="${OPTARG}"
+        SOURCEPATH="${OPTARG}"
         ;;
     b)
-        BUILDPATH="${OPTARG}"
+        WORKPATH="${OPTARG}"
+        ;;
+    *)
         ;;
     esac
 done
 
-BUILD_DATE="$(date +%Y%m%d)"
-
-# Honours the MESON variable set by the environment before setting it manually.
-
-if [ -z "$MESON" ]; then
-    MESON="$SOURCEROOT/meson.sh"
+# Creates temp dir if needed
+if [ -z "$WORKPATH" ]; then
+    WORKPATH="$(mktemp -d)"
 fi
 
+BUILD_DATE="$(date +%Y%m%d)"
+BUILDPATH="$WORKPATH/builddir"
+
+# Honours the MESON variable set by the environment before setting it manually
+
+if [ -z "$MESON" ]; then
+    MESON="$SOURCEPATH/meson.sh"
+fi
 
 # Output configured variables
 
-echo "SOURCE ROOT:        $SOURCEROOT"
-echo "BUILD ROOT:         $BUILDPATH"
-echo "NIGHTLY:            $NIGHTLY"
-echo "BUILDTYPE:          $BUILDTYPE"
-echo "MESON WRAPPER PATH: $MESON"
+echo "SCRIPT WORKING PATH: $WORKPATH"
+echo "SOURCE PATH:         $SOURCEPATH"
+echo "BUILD PATH:          $BUILDPATH"
+echo "NIGHTLY:             $NIGHTLY"
+echo "BUILDTYPE:           $BUILDTYPE"
+echo "MESON WRAPPER PATH:  $MESON"
 
-export DESTDIR="$(readlink -mf "$BUILDPATH")/dist/Naev.AppDir"
+# Make temp directories
+mkdir -p "$WORKPATH"/{dist,utils,AppDir}
+
+DESTDIR="$(readlink -mf "$WORKPATH")/AppDir"
+export DESTDIR
+
+# Get linuxdeploy's AppImage
+linuxdeploy="$WORKPATH/utils/linuxdeploy-x86_64.AppImage"
+if [ ! -f "$linuxdeploy" ]; then
+    curl -L -o "$linuxdeploy" https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
+    #
+    # This fiddles with some magic bytes in the ELF header. Don't ask me what this means.
+    # For the layman: makes appimages run in docker containers properly again.
+    # https://github.com/AppImage/AppImageKit/issues/828
+    #
+    sed '0,/AI\x02/{s|AI\x02|\x00\x00\x00|}' -i "$linuxdeploy"
+    chmod +x "$linuxdeploy"
+fi
 
 # Run build
 # Setup AppImage Build Directory
-sh "$MESON" setup "$BUILDPATH" "$SOURCEROOT" \
---native-file "$SOURCEROOT/utils/build/linux.ini" \
+sh "$MESON" setup "$BUILDPATH" "$SOURCEPATH" \
+--native-file "$SOURCEPATH/utils/build/linux.ini" \
 --buildtype "$BUILDTYPE" \
 --force-fallback-for=glpk,SuiteSparse \
 -Dnightly="$NIGHTLY" \
@@ -76,39 +98,38 @@ sh "$MESON" install -C "$BUILDPATH"
 # Prep dist directory for appimage
 
 # Set ARCH of AppImage
-export ARCH=$(arch)
+ARCH=$(arch)
 
 # Set VERSION and OUTPUT variables
-if [ -f "$SOURCEROOT/dat/VERSION" ]; then
-    export VERSION="$(<"$SOURCEROOT/dat/VERSION")"
+if [ -f "$SOURCEPATH/dat/VERSION" ]; then
+    VERSION="$(<"$SOURCEPATH/dat/VERSION")"
+    export VERSION
 else
-    echo "The VERSION file is missing from $SOURCEROOT."
-    exit -1
+    echo "The VERSION file is missing from $SOURCEPATH."
+    exit 1
 fi
 
 if [[ "$BUILDTYPE" =~ "debug" ]]; then
     export VERSION="$VERSION+DEBUG.$BUILD_DATE"
 fi
 
-SUFFIX="$VERSION-linux-x86-64"
+SUFFIX="$VERSION-linux"
 
-# Make output dir (if it does not exist)
-mkdir -p "$BUILDPATH/dist"
-
-export OUTPUT="$BUILDPATH/dist/naev-$SUFFIX.AppImage"
-
-# Get linuxdeploy's AppImage
-linuxdeploy="$BUILDPATH/linuxdeploy-x86_64.AppImage"
-if [ ! -f "$linuxdeploy" ]; then
-    curl -L -o "$linuxdeploy" https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
-    #
-    # This fiddles with some magic bytes in the ELF header. Don't ask me what this means.
-    # For the layman: makes appimages run in docker containers properly again.
-    # https://github.com/AppImage/AppImageKit/issues/828
-    #
-    sed '0,/AI\x02/{s|AI\x02|\x00\x00\x00|}' -i "$linuxdeploy"
-    chmod +x "$linuxdeploy"
+if [[ "$ARCH" =~ "x86_64" ]]; then
+    SUFFIX="$SUFFIX-x86-64"
+elif [[ "$ARCH" =~ "x86" ]]; then
+    SUFFIX="$SUFFIX-x86"
+else
+    SUFFIX="$SUFFIX-unknown"
 fi
+
+export OUTPUT="$WORKPATH/dist/naev-$SUFFIX.AppImage"
+
+# Rename metainfo file
+mv "$WORKPATH/AppDir/usr/share/metainfo/org.naev.Naev.metainfo.xml" "$WORKPATH/AppDir/usr/share/metainfo/org.naev.Naev.appdata.xml"
+
+# Disable appstream test
+export NO_APPSTREAM=1
 
 # Run linuxdeploy and generate an AppDir, then generate an AppImage
 

naikari/naikari

utils/buildAppImage.sh
@@ -2,22 +2,19 @@
 
 # AppImage BUILD SCRIPT FOR NAEV
 #
-# Written by Jack Greiner (ProjectSynchro on Github: https://github.com/ProjectSynchro/)
-#
 # For more information, see http://appimage.org/
-# Pass in [-d] [-c] (set this for debug builds) [-n] (set this for nightly builds) -s <SOURCEROOT> (Sets location of source) -b <BUILDROOT> (Sets location of build directory)
+# Pass in [-d] [-c] (set this for debug builds) [-n] (set this for nightly builds) -s <SOURCEPATH> (Sets location of source) -b <WORKPATH> (Sets location of build directory)
 
-# Output destination is ${BUILDPATH}/dist
+# Output destination is ${WORKPATH}/dist
 
 set -e
 
 # Defaults
-SOURCEROOT="$(pwd)"
-BUILDPATH="$(pwd)/build/appimageBuild"
+SOURCEPATH="$(pwd)"
 NIGHTLY="false"
 BUILDTYPE="release"
 
-while getopts dcnms:b:o: OPTION "$@"; do
+while getopts dcns:b: OPTION "$@"; do
     case $OPTION in
     d)
         set -x
@@ -30,37 +27,62 @@ while getopts dcnms:b:o: OPTION "$@"; do
         BUILDTYPE="debug"
         ;;
     s)
-        SOURCEROOT="${OPTARG}"
+        SOURCEPATH="${OPTARG}"
         ;;
     b)
-        BUILDPATH="${OPTARG}"
+        WORKPATH="${OPTARG}"
+        ;;
+    *)
         ;;
     esac
 done
 
-BUILD_DATE="$(date +%Y%m%d)"
-
-# Honours the MESON variable set by the environment before setting it manually.
-
-if [ -z "$MESON" ]; then
-    MESON="$SOURCEROOT/meson.sh"
+# Creates temp dir if needed
+if [ -z "$WORKPATH" ]; then
+    WORKPATH="$(mktemp -d)"
 fi
 
+BUILD_DATE="$(date +%Y%m%d)"
+BUILDPATH="$WORKPATH/builddir"
+
+# Honours the MESON variable set by the environment before setting it manually
+
+if [ -z "$MESON" ]; then
+    MESON="$SOURCEPATH/meson.sh"
+fi
 
 # Output configured variables
 
-echo "SOURCE ROOT:        $SOURCEROOT"
-echo "BUILD ROOT:         $BUILDPATH"
-echo "NIGHTLY:            $NIGHTLY"
-echo "BUILDTYPE:          $BUILDTYPE"
-echo "MESON WRAPPER PATH: $MESON"
+echo "SCRIPT WORKING PATH: $WORKPATH"
+echo "SOURCE PATH:         $SOURCEPATH"
+echo "BUILD PATH:          $BUILDPATH"
+echo "NIGHTLY:             $NIGHTLY"
+echo "BUILDTYPE:           $BUILDTYPE"
+echo "MESON WRAPPER PATH:  $MESON"
 
-export DESTDIR="$(readlink -mf "$BUILDPATH")/dist/Naikari.AppDir"
+# Make temp directories
+mkdir -p "$WORKPATH"/{dist,utils,AppDir}
+
+DESTDIR="$(readlink -mf "$WORKPATH")/AppDir"
+export DESTDIR
+
+# Get linuxdeploy's AppImage
+linuxdeploy="$WORKPATH/utils/linuxdeploy-x86_64.AppImage"
+if [ ! -f "$linuxdeploy" ]; then
+    curl -L -o "$linuxdeploy" https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
+    #
+    # This fiddles with some magic bytes in the ELF header. Don't ask me what this means.
+    # For the layman: makes appimages run in docker containers properly again.
+    # https://github.com/AppImage/AppImageKit/issues/828
+    #
+    sed '0,/AI\x02/{s|AI\x02|\x00\x00\x00|}' -i "$linuxdeploy"
+    chmod +x "$linuxdeploy"
+fi
 
 # Run build
 # Setup AppImage Build Directory
-sh "$MESON" setup "$BUILDPATH" "$SOURCEROOT" \
---native-file "$SOURCEROOT/utils/build/linux.ini" \
+sh "$MESON" setup "$BUILDPATH" "$SOURCEPATH" \
+--native-file "$SOURCEPATH/utils/build/linux.ini" \
 --buildtype "$BUILDTYPE" \
 --force-fallback-for=glpk,SuiteSparse \
 -Dnightly="$NIGHTLY" \
@@ -76,14 +98,15 @@ sh "$MESON" install -C "$BUILDPATH"
 # Prep dist directory for appimage
 
 # Set ARCH of AppImage
-export ARCH=$(arch)
+ARCH=$(arch)
 
 # Set VERSION and OUTPUT variables
-if [ -f "$SOURCEROOT/dat/VERSION" ]; then
-    export VERSION="$(<"$SOURCEROOT/dat/VERSION")"
+if [ -f "$SOURCEPATH/dat/VERSION" ]; then
+    VERSION="$(<"$SOURCEPATH/dat/VERSION")"
+    export VERSION
 else
-    echo "The VERSION file is missing from $SOURCEROOT."
-    exit -1
+    echo "The VERSION file is missing from $SOURCEPATH."
+    exit 1
 fi
 
 if [[ "$NIGHTLY" =~ "true" ]]; then
@@ -96,27 +119,25 @@ if [[ "$BUILDTYPE" =~ "debug" ]]; then
     export VERSION="$VERSION+DEBUG.$BUILD_DATE"
 fi
 
-SUFFIX="$VERSION-linux-x86-64"
+SUFFIX="$VERSION-linux"
 
-# Make output dir (if it does not exist)
-mkdir -p "$BUILDPATH/dist"
-
-export OUTPUT="$BUILDPATH/dist/naikari-$SUFFIX.AppImage"
-
-# Get linuxdeploy's AppImage
-linuxdeploy="$BUILDPATH/linuxdeploy-x86_64.AppImage"
-if [ ! -f "$linuxdeploy" ]; then
-    curl -L -o "$linuxdeploy" https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
-    #
-    # This fiddles with some magic bytes in the ELF header. Don't ask me what this means.
-    # For the layman: makes appimages run in docker containers properly again.
-    # https://github.com/AppImage/AppImageKit/issues/828
-    #
-    sed '0,/AI\x02/{s|AI\x02|\x00\x00\x00|}' -i "$linuxdeploy"
-    chmod +x "$linuxdeploy"
+if [[ "$ARCH" =~ "x86_64" ]]; then
+    SUFFIX="$SUFFIX-x86-64"
+elif [[ "$ARCH" =~ "x86" ]]; then
+    SUFFIX="$SUFFIX-x86"
+else
+    SUFFIX="$SUFFIX-unknown"
 fi
 
-export UPDATE_INFORMATION="gh-releases-zsync|naev|naev|$TAG|naev-*.AppImage.zsync"
+export OUTPUT="$WORKPATH/dist/naikari-$SUFFIX.AppImage"
+
+# Rename metainfo file
+mv "$WORKPATH/AppDir/usr/share/metainfo/io.github.naikari.Naikari.metainfo.xml" "$WORKPATH/AppDir/usr/share/metainfo/io.github.naikari.Naikari.appdata.xml"
+
+# Disable appstream test
+export NO_APPSTREAM=1
+
+export UPDATE_INFORMATION="gh-releases-zsync|naikari|naikari|$TAG|naikari-*.AppImage.zsync"
 
 # Run linuxdeploy and generate an AppDir, then generate an AppImage
 