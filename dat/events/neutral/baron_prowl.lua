e669a5f0357b4c67c5347c8e72adf3c796f13248
diligentcircle <diligentcircle@riseup.net>
2022-02-23 00:32:02 -0500

    Fixed Baron Prowl happening alongside Baron missions.
    Some time ago I decided to change its method to claims, but forgot
    to actually insert the claim. Oops! This fixes that, and also uses
    the resulting code change to simplify a bit (don't need to constantly
    make API calls to get the planet when we've got it cached in a
    variable).

--- dat/events/neutral/baron_prowl.lua
+++ dat/events/neutral/baron_prowl.lua
@@ -17,20 +17,24 @@
 --]]
 
 function create()
+    pla, sys = planet.get("Ulios")
+    if not evt.claim(sys) then
+        evt.finish(false)
+    end
+
     baronship = pilot.add("Proteron Kahan", "Civilian",
-         planet.get("Ulios"):pos() + vec2.new(-400,-400), _("Pinnacle"),
-         {ai="trader"})
+            pla:pos() + vec2.new(-400,-400), _("Pinnacle"), {ai="trader"})
     baronship:setInvincible(true)
     baronship:setFriendly()
     baronship:setSpeedLimit(100)
     baronship:control()
-    baronship:moveto(planet.get("Ulios"):pos() + vec2.new( 500, -500), false, false)
+    baronship:moveto(pla:pos() + vec2.new( 500, -500), false, false)
     hook.pilot(baronship, "idle", "idle")
 end
 
 function idle()
-    baronship:moveto(planet.get("Ulios"):pos() + vec2.new( 500,  500), false, false)
-    baronship:moveto(planet.get("Ulios"):pos() + vec2.new(-500,  500), false, false)
-    baronship:moveto(planet.get("Ulios"):pos() + vec2.new(-500, -500), false, false)
-    baronship:moveto(planet.get("Ulios"):pos() + vec2.new( 500, -500), false, false)
+    baronship:moveto(pla:pos() + vec2.new( 500,  500), false, false)
+    baronship:moveto(pla:pos() + vec2.new(-500,  500), false, false)
+    baronship:moveto(pla:pos() + vec2.new(-500, -500), false, false)
+    baronship:moveto(pla:pos() + vec2.new( 500, -500), false, false)
 end
