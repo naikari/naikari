naev/naev

Make sure temporary ISO image is stored in /tmp

extras/macos/createDMG.sh
@@ -59,5 +59,5 @@ cp -r "$SOURCEPATH"/extras/macos/dmg_assets/. "$WORKPATH"
 cp -r "$BUILDPATH"/dist/Naev.app "$WORKPATH"
 
 # Generate ISO image and compress into DMG
-genisoimage -V Naev -D -R -apple -no-pad -o "$BUILDPATH"/naev.iso "$WORKPATH"
-dmg "$BUILDPATH"/naev.iso "$BUILDPATH"/dist/naev.dmg 
+genisoimage -V Naev -D -R -apple -no-pad -o "$WORKPATH"/naev.iso "$WORKPATH"
+dmg "$WORKPATH"/naev.iso "$BUILDPATH"/dist/naev.dmg 

naikari/naikari

extras/macos/createDMG.sh
@@ -59,5 +59,5 @@ cp -r "$SOURCEPATH"/extras/macos/dmg_assets/. "$WORKPATH"
 cp -r "$BUILDPATH"/dist/Naikari.app "$WORKPATH"
 
 # Generate ISO image and compress into DMG
-genisoimage -V Naikari -D -R -apple -no-pad -o "$BUILDPATH"/naikari.iso "$WORKPATH"
-dmg "$BUILDPATH"/naikari.iso "$BUILDPATH"/dist/naikari.dmg
+genisoimage -V Naikari -D -R -apple -no-pad -o "$WORKPATH"/naikari.iso "$WORKPATH"
+dmg "$WORKPATH"/naikari.iso "$BUILDPATH"/dist/naikari.dmg