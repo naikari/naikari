11e4baa0b24b13d6bb739419206e95eaa3915e14
diligentcircle <diligentcircle@riseup.net>
2022-09-28 11:50:16 -0400

    Added news entries for the tutorials, properly handle mhint tags.

--- dat/events/news.lua
+++ dat/events/news.lua
@@ -34,6 +34,33 @@ local fmt = require "fmt"
 
 mhint_articles = {
    {
+      title = _("Em 1 Offers Business Opportunities"),
+      text = _([[Analysts have noted tremendous opportunities on Em 1 (Hakoi system). "It's a great start for new pilots," a top analyst said. "Safe, and with many business opportunities."]]),
+      mission = "Tutorial Part 2",
+      faction = "Empire",
+      cond = function()
+         return planet.cur() ~= planet.get("Em 1")
+      end
+   },
+   {
+      title = _("Em 1 Remains a Great Start"),
+      text = _([[Analysts continue to proudly recommend Em 1 (Hakoi system) to new pilots seeking good payment. "What better way to start your piloting journey than in a safe, prosperous system like Hakoi?" a top analyst noted.]]),
+      mission = "Tutorial Part 3",
+      faction = "Empire",
+      cond = function()
+         return planet.cur() ~= planet.get("Em 1")
+      end
+   },
+   {
+      title = _("Em 5 Experiences New Growth"),
+      text = _([[Economists note an uptick in economic activity and recommend all pilots seek out new opportunities on Em 5 (Hakoi system). Safe but prosperous, the Hakoi system is tough to beat, and its prosperity is expected to continue for the foreseeable future.]]),
+      mission = "Tutorial Part 4",
+      faction = "Empire",
+      cond = function()
+         return planet.cur() ~= planet.get("Em 5")
+      end
+   },
+   {
       title = _("Empire Seeks New Recruits"),
       text = _([[The Empire is on the lookout for talented new recruits into its shipping division. "The Empire offers good pay and fantastic opportunities," a spokesperson said. "It doesn't hurt to approach one of the Empire Lieutenants seeking talent. Join the Empire today!"]]),
       mission = "Empire Recruitment",
@@ -97,9 +124,12 @@ end
 function generate_article()
    local avail_mhint_articles = {}
    for i, a in ipairs(mhint_articles) do
+      local tag = string.format("mhint_%s", a.mission)
+      local existing_articles = news.get(tag) or {}
       if not player.misnDone(a.mission) and not player.misnActive(a.mission)
             and (a.done == nil or player.misnDone(a.done))
-            and (a.cond == nil or a.cond()) then
+            and (a.cond == nil or a.cond()
+            and #existing_articles <= 0) then
          avail_mhint_articles[#avail_mhint_articles + 1] = a
       end
    end
@@ -108,6 +138,8 @@ function generate_article()
       var.push("_news_last", time.get():tonumber())
       local rmdate = time.get() + time.create(0, 360, 0)
       local a = avail_mhint_articles[rnd.rnd(1, #avail_mhint_articles)]
-      news.add(a.faction, a.title, a.text, rmdate)
+      local tag = string.format("mhint_%s", a.mission)
+      local article = news.add(a.faction, a.title, a.text, rmdate)
+      article:bind(tag)
    end
 end
