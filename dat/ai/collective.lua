920727acae56989e4deea31d6c63a1d18bf00d5b
diligentcircle <diligentcircle@riseup.net>
2022-12-19 21:46:23 -0500

    Made it possible to get refueled by Collective if friendly.

--- dat/ai/collective.lua
+++ dat/ai/collective.lua
@@ -7,10 +7,27 @@ mem.aggressive = true
 mem.land_planet = false
 
 function create ()
-   local sprice = ai.pilot():ship():price()
+   local p = ai.pilot()
+   local sprice = p:ship():price()
    mem.kill_reward = rnd.rnd(0.25 * sprice, 1 * sprice)
 
-   mem.loiter = 3 -- This is the amount of waypoints the pilot will pass through before leaving the system
+   -- No response by default.
    mem.comm_no = _("No response.")
+
+   -- Refuel available if the player is at least neutral to them.
+   local pp = player.pilot()
+   if pp:exists() then
+      local standing = ai.getstanding(pp) or -1
+      if standing >= 0 then
+         mem.comm_no = nil
+         mem.bribe_no = fmt.f(_("{pilot} does not respond."), {pilot=p:name()})
+         mem.refuel = 0
+         mem.refuel_msg = _("\"Fuel request accepted. Approaching for fuel transfer.\"")
+         mem.refuel_cannot = _("\"Refuel request declined. Insufficient fuel available.\"")
+      end
+   end
+
+   mem.loiter = 3 -- This is the amount of waypoints the pilot will pass through before leaving the system
+
    create_post()
 end
