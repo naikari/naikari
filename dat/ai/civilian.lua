6077f0c47107b63ed4e6dcb1af8a1f9ab838aae3
diligentcircle <diligentcircle@riseup.net>
2022-12-19 20:43:29 -0500

    Made it so that civilians and traders will always refuel you if needed.
    This is to make it entirely impossible to get stuck on Xeric. There
    are traders there, and if you have no credits and can't make a jump,
    they will now always offer you fuel for free.

--- dat/ai/civilian.lua
+++ dat/ai/civilian.lua
@@ -18,12 +18,16 @@ function create()
    mem.bribe_no = bribe_msg[rnd.rnd(1, #bribe_msg)]
 
    -- Refuel
-   mem.refuel = rnd.rnd(1000, 3000)
+   mem.refuel = math.min(rnd.rnd(1000, 3000), player.credits())
    local p = player.pilot()
    if p:exists() then
-      mem.refuel_msg = fmt.f(
-            _("\"I'll supply your ship with fuel for {credits}.\""),
-            {credits=fmt.credits(mem.refuel)})
+      if mem.refuel > 0 then
+         mem.refuel_msg = fmt.f(
+               _("\"I'll supply your ship with fuel for {credits}.\""),
+               {credits=fmt.credits(mem.refuel)})
+      else
+         mem.refuel_msg = _("\"Alright, I'll give you some fuel.\"")
+      end
    end
 
    mem.loiter = 3 -- This is the amount of waypoints the pilot will pass through before leaving the system
