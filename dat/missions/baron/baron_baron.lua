14bb0aba6149557ec5b8b4f22c2b215dceba9638
diligentcircle <diligentcircle@riseup.net>
2022-12-21 14:35:28 -0500

    Baron mission: recommend that the player use a local jump.

--- dat/missions/baron/baron_baron.lua
+++ dat/missions/baron/baron_baron.lua
@@ -47,7 +47,7 @@ You're halfway through your pre-flight security checks when the three appear in 
 
 chase_text = _([[As it turns out, they didn't. They have only just reached the docking bridge leading into your ship when several armed Dvaered security forces come bursting into the docking hangar. They spot the three agents and immediately open fire. One of them goes down, the others hurriedly push the crate over the bridge towards your ship. Despite the drastic change in the situation, you have time to note that the Dvaered seem more interested in punishing the criminals than retrieving their possession intact.
 
-The second agent is caught by a Dvaered bullet, and topples off the docking bridge and into the abyss below. The third manages to get the cart with the chest into your airlock before catching a round with his chest as well. As the Dvaered near your ship, you seal the airlock, fire up your engines and punch it out of the docking hangar.]])
+The second agent is caught by a Dvaered bullet, and topples off the docking bridge and into the abyss below. The third manages to get the cart with the chest into your airlock before catching a round with his chest as well. As the Dvaered near your ship, you seal the airlock, fire up your engines and punch it out of the docking hangar. Once you get into space, you may want to use a local jump to get away from Varia before the security forces blow you to bits.]])
 
 baron_hail_text = _([[Your comm is answered by a communications officer on the bridge of the Pinnacle. You tell her you've got a delivery for the baron. She runs a few checks on a console off the screen, then tells you you've been cleared for docking and that the Pinnacle will be brought to a halt.]])
 
