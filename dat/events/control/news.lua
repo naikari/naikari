b7ac018e266e0270e135de9fecf0ad0ad11bba8f
diligentcircle <diligentcircle@riseup.net>
2023-01-16 18:35:15 -0500

    Restructured the news event to allow arbitrary conditional articles.

--- dat/events/control/news.lua
+++ dat/events/control/news.lua
@@ -32,88 +32,105 @@
 local fmt = require "fmt"
 
 
-mhint_articles = {
+cond_articles = {
    {
       title = _("Missing Girl in Goddard"),
       text = _([[A distraught father seeks his missing daughter in the Goddard system. He is offering a substantial award to any private investigator willing to find her and bring her home safely."]]),
+      tag = "mhint_The Search for Cynthia",
       mission = "The Search for Cynthia",
       done = "The Runaway",
    },
    {
       title = _("Criminal Activity Rumors in The Wringer"),
       text = _([[Rumors abound that The Wringer, a small neglected station in Sirius space, is crawling with dangerous criminals, including crime lords and assassins. Sirius authorities, however, claim that these rumors are unfounded. "People exaggerate," one Sirius officer said. "The Wringer is a hellhole where petty criminals are common, but it's not exactly teeming with professional assassins. Any truly dangerous criminal knows that there's nothing for them at that awful place."]]),
+      tag = "mhint_Sirius Bounty",
       mission = "Sirius Bounty",
       cond = function()
          return (faction.get("Sirius"):playerStanding() >= 0)
-      end
+      end,
    },
    {
       title = _("Mysterious Woman Spotted"),
       text = _([[A mysterious woman has been spotted scouting the area within and around the Klantar system in Dvaered space. When approached by the media and asked who or what she is searching for, she declined to comment.]]),
+      tag = "mhint_Shadowrun",
       mission = "Shadowrun",
    },
    {
       title = _("Assassins in Alteris"),
       text = _([[Law-abiding traders are warned to stay clear of the Alteris system due to an influx of assassins in the area. Authorities suspect that corrupt businessmen are hiring these assassins in an effort to illegally stifle competition. An Imperial investigation is underway.]]),
+      tag = "mhint_Hitman 2",
       mission = "Hitman 2",
    },
    {
       title = _("Em 1 Offers Business Opportunities"),
       text = _([[Analysts have noted tremendous opportunities on Em 1 (Hakoi system). "It's a great start for new pilots," a top analyst said. "Safe, and with many business opportunities."]]),
+      tag = "mhint_Tutorial Part 2",
       mission = "Tutorial Part 2",
       cond = function()
          return planet.cur() ~= planet.get("Em 1")
-      end
+      end,
    },
    {
       title = _("Em 1 Remains a Great Start"),
       text = _([[Analysts continue to proudly recommend Em 1 (Hakoi system) to new pilots seeking good payment. "What better way to start your piloting journey than in a safe, prosperous system like Hakoi?" a top analyst noted.]]),
+      tag = "mhint_Tutorial Part 3",
       mission = "Tutorial Part 3",
       done = "Tutorial Part 2",
       cond = function()
          return planet.cur() ~= planet.get("Em 1")
-      end
+      end,
    },
    {
       title = _("Em 5 Experiences New Growth"),
       text = _([[Economists note an uptick in economic activity and recommend all pilots seek out new opportunities on Em 5 (Hakoi system). Safe but prosperous, the Hakoi system is tough to beat, and its prosperity is expected to continue for the foreseeable future.]]),
+      tag = "mhint_Tutorial Part 4",
       mission = "Tutorial Part 4",
       done = "Tutorial Part 3",
       cond = function()
          return planet.cur() ~= planet.get("Em 5")
-      end
+      end,
    },
    {
       title = _("Empire Seeks New Recruits"),
       text = _([[The Empire is on the lookout for talented new recruits into its shipping division. "The Empire offers good pay and fantastic opportunities," a spokesperson said. "It doesn't hurt to approach one of the Empire Lieutenants seeking talent. Join the Empire today!"]]),
+      tag = "mhint_Empire Recruitment",
       mission = "Empire Recruitment",
       done = "Tutorial Part 4",
    },
    {
       title = _("Enthusiasts Organize Racing"),
       text = _([[A group of enthusiasts have begun to organize informal racing events all across the galaxy. "Just a hobby," one of the organizers said, "but there's a small prize if you win. Racing is a lot of fun!" Those wishing to attend a race are encouraged to seek out organizers at the Spaceport Bar.]]),
+      tag = "mhint_Racing Skills 1",
       mission = "Racing Skills 1",
    },
    {
       title = _("Melendez Sponsors Race Events"),
-      text = _([[Races organized by a group of enthusiasts have really caught on all across the galaxy, leading to sponsorship of huge sums of prize money from Melendez Corporation. "It's exciting," one of the organizers said. "We still run the casual races for people who are new to the sport, but Melendez really offers a big check to the winners of the sponsored races! Competition is high, and that's something I really like seeing." Those wishing to attend a race are encouraged to seek out organizers at the Spaceport Bar.]]),
+      text = _([[Races organized by a group of enthusiasts have really caught on all across the galaxy, leading to sponsorship of huge sums of prize money from Melendez Corporation. "It's exciting," one of the organizers said. "We still run the casual races for people who are new to the sport, but Melendez really offers a big check to the winners of the competitive races! Competition is high, and that's something I really like seeing." Those wishing to attend a race are encouraged to seek out organizers at the Spaceport Bar.]]),
+      tag = "mhint_Racing Skills 2",
       mission = "Racing Skills 2",
       done = "Racing Skills 1",
       cond = function()
          return not var.peek("racing_done")
-      end
+      end,
+      delcond = function()
+         return var.peek("racing_done")
+      end,
    },
    {
       title = _("Mercenary Registrations Skyrocket"),
       text = _([[Many pilots are realizing just how lucrative the life of a mercenary can be. "Missions that require a Mercenary License pay a lot of money," one analyst explained. "The cost of the Mercenary License may seem high, but it will easily pay for itself ten times over by granting access to so many lucrative missions. Not to mention, doing mercenary missions for militaries around the galaxy is a quick way to gain access to restricted areas." A Mercenary License can be obtained simply by purchasing it at any outfitter, just like all other licenses.]]),
-      mission = "Patrol",
+      tag = "mhint_Patrol",
       cond = function()
          return (player.numOutfit("Mercenary License") <= 0)
-      end
+      end,
+      delcond = function()
+         return (player.numOutfit("Mercenary License") > 0)
+      end,
    },
    {
       title = _("Dvaered and FLF Clash"),
       text = _([[An increased incidence of confrontation between Dvaered and FLF forces has been reported as of late in the north edge of Dvaered space, particularly between Frontier space and the Outer Nebula. Civilians in the area are advised to be on high alert.]]),
+      tag = "mhint_Take the Dvaered crew home",
       mission = "Take the Dvaered crew home",
       cond = function()
          return (faction.get("Dvaered"):playerStanding() >= 0
@@ -121,17 +138,24 @@ mhint_articles = {
             and not player.misnDone("Deal with the FLF agent")
             and not player.misnActive("Deal with the FLF agent")
             and player.numOutfit("Mercenary License") > 0)
-      end
+      end,
+      delcond = function()
+         return player.misnDone("Deal with the FLF agent")
+      end,
    },
    {
       title = _("Za'lek Students Test Engine Technology"),
       text = _([[Students all over Za'lek space have been designing new experimental engine designs as part of a new government funded program. These students seek pilots willing to test fly their experimental engine designs for a substantial sum of credits.]]),
+      tag = "mhint_Za'lek Test",
       mission = "Za'lek Test",
       cond = function()
          return (player.numOutfit("Mercenary License") > 0
             and faction.playerStanding("Za'lek") >= 5
             and not var.peek("zalek_test_done"))
-      end
+      end,
+      delcond = function()
+         var.peek("zalek_test_done")
+      end,
    },
 }
 
@@ -180,11 +204,16 @@ function cleanup_articles()
    end
 
    -- Remove mission hints that aren't relevant anymore.
-   for i, at in ipairs(mhint_articles) do
-      if player.misnDone(at.mission) then
-         local tag = string.format("mhint_%s", at.mission)
-         for j, a in ipairs(news.get(tag)) do
-            a:rm()
+   for i, at in ipairs(cond_articles) do
+      if (at.mission ~= nil and player.misnDone(at.mission))
+            or (at.delcond ~= nil and at.delcond()) then
+         if at.tag ~= nil then
+            for j, a in ipairs(news.get(at.tag)) do
+               a:rm()
+            end
+         else
+            warn(fmt.f(_("Conditional news entry {index} ({title}) has no tag!"),
+                  {index=i, title=at.title}))
          end
       end
    end
@@ -192,26 +221,31 @@ end
 
 
 function generate_article()
-   local avail_mhint_articles = {}
-   for i, a in ipairs(mhint_articles) do
-      local tag = string.format("mhint_%s", a.mission)
-      local existing_articles = news.get(tag) or {}
-      if not player.misnDone(a.mission) and not player.misnActive(a.mission)
-            and (a.done == nil or player.misnDone(a.done))
-            and (a.cond == nil or a.cond())
-            and (a.faction == nil or a.faction == "Generic"
-               or faction.get(a.faction) == planet.cur():faction())
-            and #existing_articles <= 0 then
-         avail_mhint_articles[#avail_mhint_articles + 1] = a
+   local avail_cond_articles = {}
+   for i, a in ipairs(cond_articles) do
+      if a.tag ~= nil then
+         local existing_articles = news.get(a.tag) or {}
+         if #existing_articles <= 0
+               and (a.mission == nil
+                  or (not player.misnDone(a.mission)
+                     and not player.misnActive(a.mission)))
+               and (a.done == nil or player.misnDone(a.done))
+               and (a.cond == nil or a.cond())
+               and (a.faction == nil or a.faction == "Generic"
+                  or faction.get(a.faction) == planet.cur():faction()) then
+            avail_cond_articles[#avail_cond_articles + 1] = a
+         end
+      else
+         warn(fmt.f(_("Conditional news entry {index} ({title}) has no tag!"),
+               {index=i, title=a.title}))
       end
    end
 
-   if #avail_mhint_articles > 0 then
+   if #avail_cond_articles > 0 then
       var.push("_news_last", time.get():tonumber())
       local rmdate = time.get() + time.create(0, 120, 0)
-      local a = avail_mhint_articles[rnd.rnd(1, #avail_mhint_articles)]
-      local tag = string.format("mhint_%s", a.mission)
+      local a = avail_cond_articles[rnd.rnd(1, #avail_cond_articles)]
       local article = news.add(a.faction or "Generic", a.title, a.text, rmdate)
-      article:bind(tag)
+      article:bind(a.tag)
    end
 end
