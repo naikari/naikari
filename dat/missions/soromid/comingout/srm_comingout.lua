467b414a4b5b46597abdbb9168c214c47527e289
diligentcircle <diligentcircle@riseup.net>
2023-03-13 20:22:17 -0400

    Started touching up the first Coming Out mission.

--- dat/missions/soromid/comingout/srm_comingout.lua
+++ dat/missions/soromid/comingout/srm_comingout.lua
@@ -34,30 +34,29 @@
 
 --]]
 
+local fmt = require "fmt"
 require "numstring"
 require "missions/soromid/common"
 
 
-ask_text = _([[The stranger looks up at you as you approach. Their voice is deeper than you expected from their appearance as they nervously speak.
+ask_text = _([[The stranger looks up at you as you approach. Their voice is deeper than you expected from their appearance as they nervously speak. "Oh! Uh, h-hi! Nice to meet you. I'm… gee, I was just thinking of what name to go by, what are the odds?" They let out a nervous laugh. "I suppose I'll go by 'C' for now since I'm not sure yet about the name. I, uh, definitely don't want to give out my deadname anymore. They/them pronouns please." You likewise introduce yourself and, taking their lead, note your own pronouns. "Say, {player}, you're a pilot, aren't you? Maybe you can help me out.
 
-"Oh! Uh, h-hi! Nice to meet you. I'm... gee, I was just thinking of what name to go by, what are the odds?" They let out a nervous laugh. "I suppose I'll go by 'C' for now since I'm not sure yet about the name. I, uh, definitely don't want to give out my deadname anymore. They/them pronouns please." You likewise introduce yourself and, taking their lead, note your own pronouns. They seem like a nice person, so you start chatting. You learn that C recently found out that they're transgender and was just now trying to sort out their feelings about their gender. At some point, the fact that you're a pilot comes up, which causes C to perk up. "Oh! Maybe you could help me with my other problem, then!
+"See, I was running a few errands here in Soromid space, but I don't have my piloting license yet, so I hired someone else to give me a ride. Well, turns out I started finding out that I'm transgender at the worst possible time. I started changing my appearance ever so slightly and he suddenly started being a transmisic asshole to me, constantly going on about how I'll 'never be a real woman', calling me a "pervert", "sick", and pretty much every other shitty thing you can think of for weeks on end until he dumped me here on this bar yesterday. 'Just a few minutes,' he said. Well, clearly he isn't coming back. I was going to look for a new pilot after I picked a name, but you seem really nice. Could you be my transport? I would just need you to take me to a series of points in Soromid space. I can give you the same payment I was going to give to my previous transport, {credits}. Does that sound like something you'd be interested in doing?"]])
 
-"See, I was running a few errands for my parents here in Soromid space, but I don't have my piloting license yet, so I hired someone else to give me a ride. He said something about needing to get some 'personal things' sorted and that he would be back in just a few minutes. Well, it's been almost an hour now and he hasn't come back. I was going to look for a new pilot after sorting out my feelings about my gender for a bit, but you seem really nice. Could you be my transport? I would just need you to take me to a series of points in Soromid space. I haven't paid my previous transport yet, so I can give the payment to you instead. Does that sound like something you'd be interested in doing?"]])
+no_text = _([["Oh, OK. I guess you must be busy. No worries, I'll try to find someone else later."]])
 
-no_text = _([["Oh, ok.... I guess you must be busy. No worries, I'll try to find someone else later."]])
+ask_again_text = _([["Oh! Hi again, {player}! I still could use your help getting around to some places in Soromid space in exchange for {credits}. What do you say?"]])
 
-ask_again_text = _([["Oh! Hi again! I still could use your help getting around to some places in Soromid space. What do you say?"]])
-
-yes_text = _([[C seems relieved when you tell them that you'd be happy to help transport them. "Thank you so much," they say. They show you the destinations they need to go to so you can input them on your mission computer. "The order doesn't matter," they say. "I just need to get to all of these destinations, and then I need a transport to my home planet, %s."]])
+yes_text = _([[C seems relieved when you tell them that you'd be happy to help transport them. "Thank you so much," they say. They show you the destinations they need to go to so you can input them on your mission computer. "The order doesn't matter," they say. "I just need to get to all of these destinations, and then I need a transport to my home planet, {planet}." C shudders. "I sure hope my parents don't act like that asshole did.… Anyway, I'll be waiting on your ship."]])
 
 dest_text = {}
-dest_text[1] = _([[You arrive at your first destination after a trip that involved a pretty sizable amount of conversation. "Thank you for helping me with this," C says as you begin to approach. "And thanks for, um, not ignoring the pronouns I requested." Taken aback, you tell C that it's how you expect to be treated and that you wouldn't want to deny someone else's humanity by not recognizing something as fundamental to themself as their gender. C smiles at this.
+dest_text[1] = _([[You arrive at your first destination after a trip that involved a pretty sizable amount of conversation. "Thank you for helping me with this," C says as you begin to approach. "And thanks for, um, treating me like a human being." You can't help but laugh slightly at the statement as you remark on how incredibly low the bar must be for you to be thanked for that. C laughs along with you.
 
-When you land, C takes off and goes to do whatever they need to do. By the time you've finished docking procedures, they return with a cheery look on their face. "Someone just called me 'ma'am'!" they explain excitedly. "That's the first time! I don't know what it is. I guess part of it might have been because I didn't speak. Either way, it felt really validating." You congratulate them. "Thank you! You know what? I'm going to start using she/her pronouns, I think." You give a nod and a smile, and mentally note to yourself to modify how you refer to her from now on.]])
+When you land, C takes off and goes to do whatever they need to do. By the time you've finished docking procedures, they return with a cheery look on their face. "Someone just called me 'ma'am'!" they explain excitedly. "That's the first time! I don't know what it is. I guess part of it might have been because I didn't speak. Either way, it felt really validating." You congratulate them. "Thank you! You know what? I'm going to start using she/her pronouns, I think." You give a nod and thank her for telling you.]])
 
-dest_text[2] = _([[You have many long conversations with C about a number of topics during the trip to this location, including piloting. At some point during the trip, your experience as a pilot came up and you told C about your adventures. As you begin boarding, C speaks up again. "You know, you're an inspiration." You feel yourself blushing a little. She continues. "To travel all the way from Empire space into Soromid space really is quite huge! And all those places you've been to along the way! You're exactly the kind of pilot I want to be." You only half-jokingly suggest that maybe they should collaborate on a mission when she gets her piloting license. She softly giggles.
+dest_text[2] = _([[You have many long conversations with C about a number of topics during the trip to this location, including piloting. At some point during the trip, your experience as a pilot came up and you told C about your adventures. "You know, you're an inspiration," C remarks as you land. You feel yourself blushing a little as she continues. "To travel all the way from Empire space into Soromid space really is quite huge! And all those places you've been to along the way! You're exactly the kind of pilot I want to be." You only half-jokingly suggest that maybe you should collaborate with her on a mission when she gets her piloting license. She softly giggles.
 
-This time, you finish docking procedures before C finishes her errands at this location, so you head off on your own for a while. When you get back, you see C has returned, looking slightly sadder than usual, so you ask how her errands went. "Oh, it was fine," she says. "It's just, someone misgendered me while I was there." You ask if she's OK. "Yeah, I'm fine," she affirms. "It wasn't intentional and they corrected themself right after. It just kind of sucks, you know? Just because I look and sound a little different from society's expectation for what a woman is, people assume I'm a man." Hearing this, you tell her that you're always available to talk with her since you've begun to view her as a friend. She smiles. "Thank you. I appreciate it."]])
+This time, you finish docking procedures before C finishes her errands at this location, so you head off on your own for a while. When you get back, you see C has returned, looking slightly sadder than usual, so you ask how her errands went. "Oh, it was fine," she says. "It's just, someone misgendered me while I was there." You ask if she's OK. "Yeah, I'm fine," she affirms. "It wasn't intentional and they corrected themself right after. It just kind of sucks, you know? Just because I look and sound a little different from society's expectation for what a woman is, people assume I'm a man." Unsure of what else to say, you tell her that you're always available to talk with her since you've begun to view her as a friend. She smiles. "Thank you. I appreciate it."]])
 
 dest_text[3] = _([[The trip to this destination was filled with conversation, and you and C have learned a lot about each other. C mentioned several names she was considering, then after some deliberation, came to an answer. "I think I like 'Chelsea'," she said.
 
@@ -114,14 +113,15 @@ function accept ()
    local txt = started and ask_again_text or ask_text
    started = true
 
-   if tk.yesno("", txt) then
-      tk.msg("", yes_text:format(homeplanet:name()))
+   if tk.yesno("", fmt.f(txt,
+         {player=player.name(), credits=fmt.credits(credits)})) then
+      tk.msg("", fmt.f(yes_text, {planet=homeplanet:name()}))
 
       misn.accept()
 
       misn.setTitle(misn_title)
       misn.setDesc(misn_desc:format(homeplanet:name()))
-      misn.setReward(creditstring(credits))
+      misn.setReward(fmt.credits(credits))
 
       generate_osd()
 
