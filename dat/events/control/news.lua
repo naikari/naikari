77a078c06d72e3e3cedd82a086258247eb6f28e0
diligentcircle <diligentcircle@riseup.net>
2023-06-02 03:37:35 -0400

    Made the Mercenary License news article only show up when you can buy one.

--- dat/events/control/news.lua
+++ dat/events/control/news.lua
@@ -147,7 +147,8 @@ cond_articles = {
       text = _([[Many pilots are realizing just how lucrative the life of a mercenary can be. "Missions that require a Mercenary License pay a lot of money," one analyst explained. "The cost of the Mercenary License may seem high, but it will easily pay for itself ten times over by granting access to so many lucrative missions. Not to mention, doing mercenary missions for militaries around the galaxy is a quick way to gain access to restricted areas." A Mercenary License can be obtained simply by purchasing it at any outfitter, just like all other licenses.]]),
       tag = "mhint_Patrol",
       cond = function()
-         return (player.numOutfit("Mercenary License") <= 0)
+         return (player.numOutfit("Mercenary License") <= 0
+               and player.credits() >= outfit.price("Mercenary License"))
       end,
       delcond = function()
          return (player.numOutfit("Mercenary License") > 0)
