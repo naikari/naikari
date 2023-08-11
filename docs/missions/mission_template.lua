ccf6a712af437d137923bf3e91a2e3d8d8f9eb7c
diligentcircle <diligentcircle@riseup.net>
2023-08-10 21:26:19 -0400

    Updated the mission template to be more up-to-date.

--- docs/missions/mission_template.lua
+++ docs/missions/mission_template.lua
@@ -13,25 +13,7 @@
 --]]
 --[[
 
-   Mission Template (mission name goes here)
-
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
---
-
-   This is a Naikari mission template.
-   In this document aims to provide a structure on which to build many
+   This document aims to provide a structure on which to build many
    Naikari missions and teach how to make basic missions in Naikari.
    Naikari missions are written in the Lua programming language.
    There is documentation on Naikari's Lua API on the Naikari website:
@@ -43,11 +25,10 @@
 
    When creating a mission with this template, please erase the
    explanatory comments (such as this one) along the way, but retain the
-   above license header and the MISSION and DESCRIPTION fields below,
-   adapted to your mission. You must also ensure you check the XML
-   portion at the top and that your mission has a unique name defined
-   there (that name is not displayed to players, but is used to identify
-   missions internally).
+   MISSION and DESCRIPTION fields below, adapted to your mission. You
+   must also ensure you check the XML portion at the top and that your
+   mission has a unique name defined there (that name is not displayed
+   to players, but is used to identify missions internally).
 
    MISSION: <NAME GOES HERE>
    DESCRIPTION: <DESCRIPTION GOES HERE>
@@ -73,6 +54,11 @@ which figures out what all the translatable text is can find it.
 allowing gettext to know about the string by using `N_()` around the
 string and then later usin `_()` on the variable that contains it.)
 
+Note the local keyword used on these variables; this means that the
+variable will not be persisted between game sessions, which is useful
+for these kinds of text variables since it ensures the player won't be
+shown outdated text.
+
 When writing dialog, write it like a book (in the present-tense), with
 paragraphs and quotations and all that good stuff. Use a double line
 break, as shown below, for new paragraphs. Use quotation marks as would
@@ -96,18 +82,18 @@ ngettext, which must be done inline. Pulling out the entire sentence is
 necessary to ensure that translators have full capability to translate
 the sentence properly.
 --]]
-ask_text = _([[As you approach the guy, he looks up in curiosity. You sit down and ask him how his day is. "Why, fine," he answers. "How are you?" You answer that you are fine as well and compliment him on his suit, which seems to make his eyes light up. "Why, thanks! It's my favorite suit! I had it custom tailored, you know.
+local ask_text = _([[As you approach the guy, he looks up in curiosity. You sit down and ask him how his day is. "Why, fine," he answers. "How are you?" You answer that you are fine as well and compliment him on his suit, which seems to make his eyes light up. "Why, thanks! It's my favorite suit! I had it custom tailored, you know.
 
-"Actually, that reminds me! There was a special suit on {planet} in the {system} system, the last one I need to complete my collection, but I don't have a ship. You do have a ship, don't you? {reward_sentence} What do you say?"]])
+"Actually, that reminds me! There was a special suit on {planet} in the {system} system, the last one I need to complete my collection, but I don't have a ship. You do have a ship, don't you? So I'll tell you what, give me a ride and I'll pay you {credits} credits for it! What do you say?"]])
 
-ask_again_text = _([["Ah, it's you again! Have you changed your mind? Like I said, I just need transport to {planet} in the {system} system."]])
+local ask_again_text = _([["Ah, it's you again! Have you changed your mind? Like I said, I just need transport to {planet} in the {system} system, and I'll pay {credits} in exchange."]])
 
-accept_text = _([["Fantastic! I knew you would do it! Like I said, I'll pay you as soon as we get there. No rush! Just bring me there when you're ready.]])
+local accept_text = _([["Fantastic! I knew you would do it! Like I said, I'll pay you as soon as we get there. No rush! Just bring me there when you're ready.]])
 
-finish_text = _([[As you arrive on %s, your passenger reacts with glee. "I must sincerely thank you, kind stranger! Now I can finally complete my suit collection, and it's all thanks to you. Here is %s, as we agreed. I hope you have safe travels!"]])
+local finish_text = _([[As you arrive on {planet}, your passenger reacts with glee. "I must sincerely thank you, kind stranger! Now I can finally complete my suit collection, and it's all thanks to you. Here is {credits}, as we agreed. I hope you have safe travels!"]])
 
-misn_desc = _("A well-dressed man wants you to take him to {planet} in the {system} system so he get some sort of special suit.")
-misn_log = _([[You helped transport a well-dressed man to %s so that he could buy some kind of special suit to complete his collection.]])
+local misn_desc = _("A well-dressed man wants you to take him to {planet} in the {system} system so he get some sort of special suit.")
+local misn_log = _([[You helped transport a well-dressed man to {planet} so that he could buy some kind of special suit to complete his collection.]])
 
 
 --[[ 
@@ -117,18 +103,24 @@ You have to set the NPC and the description. These will show up at the
 bar with the character that gives the mission and the character's
 description.
 --]]
-function create ()
-   -- Set our mission parameters.
-   misplanet, missys = planet.get("Ulios")
+function create()
+   -- Set our mission parameters. These are global variables which will
+   -- be persisted even if the game is reloaded.
+   misplanet, missys = planet.getLandable("Ulios")
    credits = 250000
    talked = false
-   
-   -- If we needed to claim a system, we would do that here with
-   -- something like the following commented out statement. However,
-   -- this mission won't be doing anything with the system itself (it
-   -- only wants us to land somewhere), so we won't make a system claim
-   -- for it.
-   --if not misn.claim(missys) then misn.finish(false) end
+
+   -- Check to make sure the mission should be given. In this case, the
+   -- only check needed is to make sure Ulios can be landed on. If it
+   -- can't be, then planet.getLandable() will have returned nil rather
+   -- than the actual planet.
+   if misplanet == nil then
+      -- Call misn.finish(false) to end the mission without marking it
+      -- as completed. This is used for mission failure where applicable
+      -- or, as it is used here, to prevent the mission from spawning in
+      -- the first place.
+      misn.finish(false)
+   end
 
    -- Give the name of the NPC and the portrait used. You can see all
    -- available portraits in artwork/gfx/portraits.
@@ -147,39 +139,22 @@ Run misn.accept() here to internally "accept" the mission. This is
 required; if you don't call misn.accept(), the mission is scrapped.
 This is also where mission details are set.
 --]]
-function accept ()
+function accept()
    -- Use different text if we've already talked to him before than if
    -- this is our first time.
    local text
    if talked then
-      -- We use `fmt.f` here to fill in the variables.
-      text = fmt.f(ask_again_text,
-            {planet=misplanet:name(), system=missys:name()})
+      text = ask_again_text
    else
-      -- And also here. However, here we include mention of how many
-      -- credits the player gets, and so it gets slightly more
-      -- complicated. First, we get the reward sentence and store it
-      -- in local variable reward_text, using fmt.f and n_() (alias to
-      -- gettext.ngettext) to translate and format this portion. Then,
-      -- we format `ask_text` as with `ask_again_text`, splicing in the
-      -- extra sentence we just translated with ngettext.
-      --
-      -- In general, credits are best indicated with ¢, but writing out
-      -- the words "credit" and "credits" as below is acceptable, too.
-      local reward_text = fmt.f(
-            n_("So I'll tell you what, give me a ride and I'll pay you {credits} credit for it!",
-               "So I'll tell you what, give me a ride and I'll pay you {credits} credits for it!",
-               credits),
-            {credits=fmt.number(credits)})
-      text = fmt.f(ask_text,
-            {planet=misplanet:name(), system=missys:name(),
-               reward_sentence=reward_text})
+      text = ask_text
       talked = true
    end
 
    -- This will create the typical "Yes/No" dialogue. It returns true if
-   -- yes was selected. 
-   if tk.yesno("", text) then
+   -- yes was selected. We use `fmt.f` here to fill in the variables.
+   if tk.yesno("", fmt.f(text,
+         {planet=misplanet:name(), system=missys:name(),
+            credits=fmt.number(credits)})) then
       -- Followup text.
       tk.msg("", accept_text)
 
@@ -205,9 +180,10 @@ function accept ()
 
       -- The OSD shows your objectives. For style purposes, the OSD
       -- description should not contain end-of-sentence punctuation.
-      local osd_desc = {}
-      osd_desc[1] = fmt.f(_("Land on {planet} ({system} system)"),
-            {planet=misplanet:name(), system=missys:name()})
+      local osd_desc = {
+         fmt.f(_("Land on {planet} ({system} system)"),
+            {planet=misplanet:name(), system=missys:name()}),
+      }
       misn.osdCreate(_("The Special Suit"), osd_desc)
 
       -- This is where we would define any other variables we need, but
@@ -227,13 +203,14 @@ end
 
 -- This is our land hook function. Once `hook.land("land")` is called,
 -- this function will be called any time the player lands.
-function land ()
+function land()
    -- First check to see if we're on our target planet.
    if planet.cur() == misplanet then
       -- Mission accomplished! Now we do an outro dialog and reward the
       -- player. Rewards are usually credits, as shown here, but
       -- other rewards can also be given depending on the circumstances.
-      tk.msg("", finish_text:format(misplanet:name(), reward_text))
+      tk.msg("", fmt.f(finish_text,
+            {planet=misplanet:name(), credits=fmt.credits(credits)}))
 
       -- Reward the player. Rewards are usually credits, as shown here,
       -- but other rewards can also be given depending on the
@@ -241,7 +218,7 @@ function land ()
       player.pay(credits)
 
       -- Add a log entry. This should only be done for unique missions.
-      addMiscLog(misn_log:format(misplanet:name()))
+      addMiscLog(fmt.f(misn_log, {planet=misplanet:name()}))
       
       -- Finish the mission. Passing the `true` argument marks the
       -- mission as complete.
