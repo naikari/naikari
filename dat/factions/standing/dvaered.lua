6a3ac845e5084caa1588b74b8a1cb56a6e3d062d
diligentcircle <diligentcircle@riseup.net>
2023-11-08 12:36:54 -0500

    Made Dvaereds ignore missions done for its allies.
    This is for lore reasons and to make the whole rep-gaining mission
    in Hakoi Pirates make more sense.

--- dat/factions/standing/dvaered.lua
+++ dat/factions/standing/dvaered.lua
@@ -6,6 +6,8 @@ _fdelta_kill = {-9, 7} -- Maximum change constraints
 
 _fthis = faction.get("Dvaered")
 
+_fmod_misn_friend = 0
+
 
 function faction_hit(current, amount, source, secondary)
     return default_hit(current, amount, source, secondary)
