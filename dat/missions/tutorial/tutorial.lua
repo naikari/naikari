b7c0d3d867960743457a663440b142f1497792b3
diligentcircle <diligentcircle@riseup.net>
2024-05-01 08:51:43 -0400

    Updated tutorial to refer to Exacorp instead of Melendez.

--- dat/missions/tutorial/tutorial.lua
+++ dat/missions/tutorial/tutorial.lua
@@ -37,9 +37,9 @@ require "events/tutorial/tutorial_common"
 require "missions/neutral/common"
 
 
-local intro_text  = _([["Welcome to space, {player}, and congratulations on your purchase," the salesperson who sold you the {shipname} says over the radio. "I am sure your new ship will serve you well! Here at Melendez Corporation, our ships are prized for their reliability and affordability. I promise, you won't be disappointed!" You barely resist the temptation to roll your eyes at the remark; you really only bought this ship because it was the only one you could afford. Still, you tactfully thank the salesperson.]])
+local intro_text  = _([["Welcome to space, {player}, and congratulations on your purchase," the salesperson who sold you the {shipname} says over the radio. "I am sure your new ship will serve you well! Here at Exacorp, our ships are prized for their reliability and affordability. I promise, you won't be disappointed!" You barely resist the temptation to roll your eyes at the remark; you really only bought this ship because it was the only one you could afford. Still, you tactfully thank the salesperson.]])
 
-local movement_text = _([["Now, so that your test flight goes as smoothly as possible, I will explain the controls of your state-of-the art Melendez Corporation starship! There are two basic modes: keyboard flight, and mouse flight.
+local movement_text = _([["Now, so that your test flight goes as smoothly as possible, I will explain the controls of your state-of-the art Exacorp starship! There are two basic modes: keyboard flight, and mouse flight.
 
 "To move via keyboard flight, rotate your ship with {leftkey} and {rightkey}, and thrust to move your ship forward with {accelkey}. You can also use {reversekey} to rotate your ship to the direction opposite of your current movement, which can be useful for bringing your vessel to a stop.
 
@@ -47,13 +47,13 @@ local movement_text = _([["Now, so that your test flight goes as smoothly as pos
 
 "Why don't you give both systems a try? Experiment with the flight controls as much as you'd like, then fly over to where {planet} is. You see it on your screen, right? It's the planet right next to you. I've hilighted it for you on your minimap."]])
 
-local landing_text = _([["I see you have a great handle on the controls of your new Melendez Corporation ship! It's a perfect fit for you, don't you think? Your control of the vessel is absolutely stunning, magnificent!
+local landing_text = _([["I see you have a great handle on the controls of your new Exacorp ship! It's a perfect fit for you, don't you think? Your control of the vessel is absolutely stunning, magnificent!
 
 "You may continue to practice flying for as long as you need. When you are ready, please land on {planet} to finalize your paperwork; you can land double-clicking on {planet} or by pressing {landkey}. I will be waiting for you at the spaceport!"]])
 
 local land_text = _([[You watch as the ship – your ship – automatically guides you safely thru the atmosphere and into the planet's space port, then touches down at an empty spot reserved for you. As soon as the hatch opens and you step out, an exhausted dock worker greets you and makes you sign a form. "Just the standard waiver," she explains. After you sign, she pushes some buttons and you stare as you see robotic drones immediately getting to work checking your ship for damage and ensuring your fuel tanks are full. Noticing your expression, the worker lets out a chuckle. "First time landing, eh?" she quips. "It'll all be normal to you before long."
 
-"Ah, there you are, {player}!" the voice of the salesperson interrupts, prompting the worker to roll her eyes and walk off. You look in the direction of the voice and see the obnoxiously dressed salesperson, wearing a huge grin. "I see your Melendez Corporation starship is serving you well. Now, if you would follow me, we can finalize that paperwork."]])
+"Ah, there you are, {player}!" the voice of the salesperson interrupts, prompting the worker to roll her eyes and walk off. You look in the direction of the voice and see the obnoxiously dressed salesperson, wearing a huge grin. "I see your Exacorp starship is serving you well. Now, if you would follow me, we can finalize that paperwork."]])
 
 local finish_text = _([[The salesperson makes you sign dozens of forms: tax forms, waivers, indemnity agreements, and much more that you aren't given enough time to process. When you finish, the salesperson pats you on the back. "You have made an excellent choice, {player}! I'm sure you'll be making millions of credits in no time.
 
@@ -62,7 +62,7 @@ local finish_text = _([[The salesperson makes you sign dozens of forms: tax form
 You figure you might as well meet this man the salesperson mentioned at the #bSpaceport Bar#0 and see if the job is worthwhile.]])
 
 misn_title = _("Point of Sale")
-misn_desc = _("You have purchased a new ship from Melendez and are in the process of finalizing the sale.")
+misn_desc = _("You have purchased a new ship from Exacorp and are in the process of finalizing the sale.")
 
 
 function create()
