dbfc7fae567c31b509967600d9c0ee25bff71a89
diligentcircle <diligentcircle@riseup.net>
2025-11-29 02:56:25 -0500

    Added Empire equip config* and finished missing Pirate config.
    * Haven't done utilities or structures yet.

--- dat/factions/equip/empire.lua
+++ dat/factions/equip/empire.lua
@@ -1,6 +1,144 @@
 require "factions/equip/generic"
 
 
+-- Probability of cargo by class.
+equip_classCargo["Yacht"] = 0.01
+equip_classCargo["Luxury Yacht"] = 0.01
+equip_classCargo["Scout"] = 0.01
+equip_classCargo["Courier"] = 0.5
+equip_classCargo["Freighter"] = 0.5
+equip_classCargo["Armored Transport"] = 0.5
+equip_classCargo["Bulk Freighter"] = 0.5
+equip_classCargo["Fighter"] = 0.01
+equip_classCargo["Bomber"] = 0.01
+equip_classCargo["Corvette"] = 0.01
+equip_classCargo["Destroyer"] = 0.01
+equip_classCargo["Cruiser"] = 0.01
+equip_classCargo["Carrier"] = 0.01
+equip_classCargo["Drone"] = 0.01
+equip_classCargo["Heavy Drone"] = 0.01
+
+
+equip_typeOutfits_coreSystems["Shark"] = {
+   "Intek Orion 2201 APU",
+}
+equip_typeOutfits_coreSystems["Lancelot"] = {
+   "Intek Orion 3301 APU",
+}
+equip_typeOutfits_coreSystems["Admonisher"] = {
+   "Intek Orion 5501 APU",
+}
+equip_typeOutfits_coreSystems["Pacifier"] = {
+   "Intek Orion 6601 APU",
+}
+equip_typeOutfits_coreSystems["Hawking"] = {
+   "Intek Orion 8801 APU",
+}
+equip_typeOutfits_coreSystems["Peacemaker"] = {
+   "Intek Orion 9901 APU",
+}
+
+equip_typeOutfits_engines["Shark"] = {
+   "Flex Tornado 150X Engine",
+}
+equip_typeOutfits_engines["Lancelot"] = {
+   "Flex Cyclone 300X Engine",
+}
+equip_typeOutfits_engines["Admonisher"] = {
+   "Flex Tornado 700X Engine",
+}
+equip_typeOutfits_engines["Pacifier"] = {
+   "Flex Cyclone 1200X Engine",
+}
+equip_typeOutfits_engines["Hawking"] = {
+   "Flex Tornado 4500X Engine",
+}
+equip_typeOutfits_engines["Peacemaker"] = {
+   "Flex Cyclone 6500X Engine",
+}
+
+equip_typeOutfits_hulls["Shark"] = {
+   "SHL Ultralight Combat Hull",
+}
+equip_typeOutfits_hulls["Lancelot"] = {
+   "SHL Light Combat Hull",
+}
+equip_typeOutfits_hulls["Admonisher"] = {
+   "SHL Medium Combat Hull",
+}
+equip_typeOutfits_hulls["Pacifier"] = {
+   "SHL Medium-Heavy Combat Hull",
+}
+equip_typeOutfits_hulls["Hawking"] = {
+   "SHL Heavy Combat Hull",
+}
+equip_typeOutfits_hulls["Peacemaker"] = {
+   "SHL Superheavy Combat Hull",
+}
+
+equip_typeOutfits_weapons["Shark"] = {
+   {
+      num = 2;
+      "FL21-U Lumina Gun",
+   },
+   {
+      "FT80-U Talon Gun",
+   },
+}
+equip_typeOutfits_weapons["Lancelot"] = {
+   {
+      "FMT40-S Tiger Missile Gun",
+   },
+   {
+      "FL27-S Lumina Gun",
+   },
+}
+equip_typeOutfits_weapons["Admonisher"] = {
+   {
+      num = 1;
+      "FMT800-H Tiger Torpedo Gun",
+      "FMT1000-H Tiger Torpedo Gun",
+   },
+   {
+      "FMT80-S Tiger Missile Gun",
+      "FMT200-H Tiger Missile Gun",
+   },
+   {
+      "FL50-H Lumina Gun",
+   },
+}
+equip_typeOutfits_weapons["Pacifier"] = {
+   {
+      "TL110-L Lumina Turret",
+   },
+   {
+      num = 1;
+      "TL54-M Lumina Turret",
+   },
+   {
+      "TMT40-M Tiger Missile Turret",
+   },
+}
+equip_typeOutfits_weapons["Hawking"] = {
+   {
+      num = 6;
+      "TL110-L Lumina Turret",
+   },
+   {
+      "TMT80-M Tiger Missile Turret",
+   },
+}
+equip_typeOutfits_weapons["Peacemaker"] = {
+   {
+      num = 4;
+      "TL200-X Lumina Turret",
+   },
+   {
+      "TMT80-M Tiger Missile Turret",
+   },
+}
+
+
 --[[
 -- @brief Does empire pilot equipping
 --
--- dat/factions/equip/pirate.lua
+++ dat/factions/equip/pirate.lua
@@ -18,30 +18,95 @@ equip_classCargo["Carrier"] = .5
 equip_classCargo["Drone"] = .1
 equip_classCargo["Heavy Drone"] = .1
 
-equip_typeOutfits_coreSystems["Hyena"] = equip_shipOutfits_coreSystems["Pirate Shark"]
-equip_typeOutfits_coreSystems["Shark"] = equip_shipOutfits_coreSystems["Pirate Shark"]
+
+equip_typeOutfits_coreSystems["Hyena"] = {
+   "Makeshift Small APU",
+}
+equip_typeOutfits_coreSystems["Shark"] = {
+   "Makeshift Small APU",
+   "Exacorp ET-200 APU",
+}
 equip_typeOutfits_coreSystems["Vendetta"] = {
+   "Makeshift Small APU",
    "Exacorp ET-300 APU",
-   "Intek Prometheus 3202 APU",
 }
 equip_typeOutfits_coreSystems["Lancelot"] = equip_typeOutfits_coreSystems["Vendetta"]
 equip_typeOutfits_coreSystems["Ancestor"] = equip_typeOutfits_coreSystems["Vendetta"]
 equip_typeOutfits_coreSystems["Phalanx"] = {
+   "Makeshift Medium APU",
    "Exacorp ET-500 APU",
-   "Intek Prometheus 5402 APU",
 }
 equip_typeOutfits_coreSystems["Admonisher"] = equip_typeOutfits_coreSystems["Phalanx"]
-equip_typeOutfits_coreSystems["Pacifier"] = {
+equip_typeOutfits_coreSystems["Rhino"] = {
+   "Makeshift Medium APU",
    "Exacorp ET-600 APU",
    "Intek Prometheus 6502 APU",
 }
 equip_typeOutfits_coreSystems["Kestrel"] = {
+   "Makeshift Large APU",
    "Exacorp ET-800 APU",
    "Intek Prometheus 8702 APU",
+   "Intek Orion 3301 APU",
 }
 
+equip_typeOutfits_engines["Hyena"] = {
+   "Beat Up Small Engine",
+}
+equip_typeOutfits_engines["Shark"] = {
+   "Beat Up Small Engine",
+   "Exacorp HS-150 Engine",
+}
+equip_typeOutfits_engines["Vendetta"] = {
+   "Beat Up Small Engine",
+   "Exacorp HS-300 Engine",
+}
+equip_typeOutfits_engines["Lancelot"] = equip_typeOutfits_engines["Vendetta"]
+equip_typeOutfits_engines["Ancestor"] = equip_typeOutfits_engines["Vendetta"]
+equip_typeOutfits_engines["Phalanx"] = {
+   "Beat Up Medium Engine",
+   "Exacorp HS-700 Engine",
+}
+equip_typeOutfits_engines["Admonisher"] = equip_typeOutfits_engines["Phalanx"]
 equip_typeOutfits_engines["Rhino"] = {
+   "Beat Up Medium Engine",
    "Exacorp HS-1200 Engine",
+   "NGL Hauler 1600F Engine",
+}
+equip_typeOutfits_engines["Kestrel"] = {
+   "Beat Up Large Engine",
+   "Exacorp HS-4500 Engine",
+   "Flex Gust 3800S Engine",
+}
+
+equip_typeOutfits_hulls["Hyena"] = {
+   "Patchwork Light Hull",
+}
+equip_typeOutfits_hulls["Shark"] = {
+   "Patchwork Light Hull",
+   "Exacorp D-2 Hull",
+}
+equip_typeOutfits_hulls["Vendetta"] = {
+   "Patchwork Light Hull",
+   "Exacorp D-4 Hull",
+}
+equip_typeOutfits_hulls["Lancelot"] = equip_typeOutfits_hulls["Vendetta"]
+equip_typeOutfits_hulls["Ancestor"] = equip_typeOutfits_hulls["Vendetta"]
+equip_typeOutfits_hulls["Phalanx"] = {
+   "Patchwork Medium Hull",
+   "Exacorp D-12 Hull",
+}
+equip_typeOutfits_hulls["Admonisher"] = equip_typeOutfits_hulls["Phalanx"]
+equip_typeOutfits_hulls["Rhino"] = {
+   "Patchwork Medium Hull",
+   "Exacorp D-24 Hull",
+   "Exacorp X-24 Hull",
+   "NGL Medium Cargo Hull",
+}
+equip_typeOutfits_hulls["Kestrel"] = {
+   "Patchwork Heavy Hull",
+   "Exacorp D-48 Hull",
+   "Exacorp X-48 Hull",
+   "SHL Heavy Combat Hull",
 }
 
 equip_typeOutfits_weapons["Hyena"] = {
